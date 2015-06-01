75fb3c2226251d55938658c9202c5a74b36be11d
Thomas Gelf <thomas@gelf.net>
2015-06-01 12:43:41 +0200

    Menu: add 'Activity Log'

--- configuration.php
+++ configuration.php
@@ -9,4 +9,6 @@ $section->add($this->translate('Commands'))
     ->setUrl('director/list/commands');
 $section->add($this->translate('Hosts'))
     ->setUrl('director/list/hosts');
-
+$section->add($this->translate('Activity Log'))
+    ->setUrl('director/list/activitylog')
+    ->setPriority(900);
