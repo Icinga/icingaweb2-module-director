26621f5ab1db2f4faa9fe4c7be5fdb5b417bbd3c
Thomas Gelf <thomas@gelf.net>
2015-06-08 13:02:09 +0200

    IcingaObject: add generic helper methods

--- library/Director/Objects/IcingaObject.php
+++ library/Director/Objects/IcingaObject.php
@@ -10,6 +10,25 @@ abstract class IcingaObject extends DbObject
 
     protected $autoincKeyName = 'id';
 
+    protected $supportsCustomVars = false;
+
+    public function supportsCustomVars()
+    {
+        return $this->supportsCustomVars;
+    }
+
+    public function isTemplate()
+    {
+        return property_exists($this, 'object_type')
+            && $this->object_type === 'template';
+    }
+
+    public function isApplyRule()
+    {
+        return property_exists($this, 'object_type')
+            && $this->object_type === 'apply';
+    }
+
     public function onInsert()
     {
         DirectorActivityLog::logCreation($this, $this->connection);
