bc00eefb07fcd1cfe61b1702a323bbb18e05d7bd
Alexander Fuhr <alexander.fuhr@netways.de>
2015-06-17 18:57:51 +0200

    Conf: renamed controller to Config

--- application/controllers/ConfigController.php
+++ application/controllers/ConfigController.php
@@ -4,15 +4,24 @@ use Icinga\Module\Director\ActionController;
 
 use Icinga\Module\Director\IcingaConfig\IcingaConfig;
 
-class Director_ConfController extends ActionController
+class Director_ConfigController extends ActionController
 {
     public function showAction()
     {
+        /** @var IcingaConfig $config */
         $config = IcingaConfig::generate($this->db());
         $this->view->files = array();
 
         foreach ($config->getFiles() as $filename => $config) {
             $this->view->files[$filename] = $config->getContent();
         }
+    }
+
+    public function storeAction()
+    {
+        /** @var IcingaConfig $config */
+        $config = IcingaConfig::generate($this->db());
+
+        $config->store();
     }
 }
