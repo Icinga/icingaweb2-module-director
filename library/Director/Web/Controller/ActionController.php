628d7c3221bbe3edee696f1ec43db49538ebb25f
Thomas Gelf <thomas@gelf.net>
2015-06-30 11:30:08 +0200

    ActionController: remove legacy tab helpers

--- library/Director/Web/Controller/ActionController.php
+++ library/Director/Web/Controller/ActionController.php
@@ -46,63 +46,6 @@ abstract class ActionController extends Controller
         return $this->view->tabs;
     }
 
-    protected function setGlobalTabs()
-    {
-        $this->view->tabs = Widget::create('tabs')->add('commands', array(
-            'label' => $this->translate('Commands'),
-            'url'   => 'director/list/commands')
-        )->add('commandarguments', array(
-            'label' => $this->translate('(args)'),
-            'url'   => 'director/list/commandarguments')
-        )->add('timeperiods', array(
-            'label' => $this->translate('Timeperiods'),
-            'url'   => 'director/list/timeperiods')
-        )->add('zones', array(
-            'label' => $this->translate('Zones'),
-            'url'   => 'director/list/zones')
-        )->add('endpoints', array(
-            'label' => $this->translate('(ep)'),
-            'url'   => 'director/list/endpoints')
-        );
-        return $this->view->tabs;
-    }
-
-    protected function setHostTabs()
-    {
-        $this->view->tabs = Widget::create('tabs')->add('hosts', array(
-            'label' => $this->translate('Hosts'),
-            'url'   => 'director/list/hosts')
-        )->add('hostgroups', array(
-            'label' => $this->translate('Hostgroups'),
-            'url'   => 'director/list/hostgroups')
-        );
-        return $this->view->tabs;
-    }
-
-    protected function setServiceTabs()
-    {
-        $this->view->tabs = Widget::create('tabs')->add('services', array(
-            'label' => $this->translate('Services'),
-            'url'   => 'director/list/services')
-        )->add('servicegroups', array(
-            'label' => $this->translate('Servicegroups'),
-            'url'   => 'director/list/servicegroups')
-        );
-        return $this->view->tabs;
-    }
-
-    protected function setUserTabs()
-    {
-        $this->view->tabs = Widget::create('tabs')->add('users', array(
-            'label' => $this->translate('Users'),
-            'url'   => 'director/list/users')
-        )->add('usergroups', array(
-            'label' => $this->translate('Usergroups'),
-            'url'   => 'director/list/usergroups')
-        );
-        return $this->view->tabs;
-    }
-
     protected function db()
     {
         if ($this->db === null) {
