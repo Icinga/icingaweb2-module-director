6ca8e8a7d612196f77b38a559472ca65cd493fab
Thomas Gelf <thomas@gelf.net>
2015-07-21 15:18:16 +0200

    ImportSourceSql: new import provider
    fixes #9678

---
+++ library/Director/Import/ImportSourceSql.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Icinga\Module\Director\Import;
+
+use Icinga\Module\Director\Web\Form\QuickForm;
+use Icinga\Module\Director\Web\Hook\ImportSourceHook;
+use Icinga\Data\Db\DbConnection;
+use Icinga\Web\Form;
+
+class ImportSourceSql extends ImportSourceHook
+{
+    protected $db;
+
+    public function fetchData()
+    {
+        return $this->db()->fetchAll($this->settings['query']);
+    }
+
+    public function listColumns()
+    {
+        return array_keys((array) current($this->fetchData()));
+    }
+
+    public static function addSettingsFormFields(QuickForm $form)
+    {
+        $form->addElement('text', 'resource', array(
+            'label'    => 'Resouce name',
+            'required' => true,
+        ));
+        $form->addElement('text', 'query', array(
+            'label'    => 'DB Query',
+            'required' => true,
+        ));
+        return $form;
+    }
+
+    protected function db()
+    {
+        if ($this->db === null) {
+            $this->db = DbConnection::fromResourceName($this->settings['resource'])->getDbAdapter();
+        }
+
+        return $this->db;
+    }
+
+}
