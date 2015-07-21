6b47e561e9f75721e08987a19900d8a72bc4d40c
Thomas Gelf <thomas@gelf.net>
2015-07-21 15:19:31 +0200

    run.php: provide new import source as a hook
    refs #9690

--- run.php
+++ run.php
@@ -1,4 +1,6 @@
 <?php
 
 $this->registerHook('Monitoring\\HostActions', '\\Icinga\\Module\\Director\\Web\\HostActions');
+$this->registerHook('Director\\ImportSource', '\\Icinga\\Module\\Director\\Import\\ImportSourceSql', 'sql');
+
 
