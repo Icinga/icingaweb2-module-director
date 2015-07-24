c868e13be56848053b941689e6f4e05ab313fd7d
Alexander Fuhr <alexander.fuhr@netways.de>
2015-07-24 14:59:53 +0200

    run.php: Register string/numer hooks
    refs #9715

--- run.php
+++ run.php
@@ -3,4 +3,5 @@
 $this->registerHook('Monitoring\\HostActions', '\\Icinga\\Module\\Director\\Web\\HostActions');
 $this->registerHook('Director\\ImportSource', '\\Icinga\\Module\\Director\\Import\\ImportSourceSql', 'sql');
 
-
+$this->registerHook('Director\\DataType', '\\Icinga\\Module\\Director\\DataType\\DataTypeString', 'string');
+$this->registerHook('Director\\DataType', '\\Icinga\\Module\\Director\\DataType\\DataTypeNumber', 'number');
