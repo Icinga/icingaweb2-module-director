3192700d3a7416b2196ad6cb46cdb09988a8d56d
Thomas Gelf <thomas@gelf.net>
2015-07-28 11:43:40 +0200

    SyncPropertyForm: coding style cleanup

--- application/forms/SyncPropertyForm.php
+++ application/forms/SyncPropertyForm.php
@@ -10,45 +10,45 @@ class SyncPropertyForm extends DirectorObjectForm
     public function setup()
     {
         $this->addElement('select', 'rule_id', array(
-            'label' => $this->translate('Rule Name'),
-            'required'    => true,
+            'label'    => $this->translate('Rule Name'),
+            'required' => true,
         ));
 
         $this->addElement('select', 'source_id', array(
-            'label' => $this->translate('Source Name'),
-            'required'    => true,
+            'label'    => $this->translate('Source Name'),
+            'required' => true,
         ));
 
        $this->addElement('text', 'source_expression', array(
-            'label' => $this->translate('Source Expression'),
-            'required'    => true,
+            'label'    => $this->translate('Source Expression'),
+            'required' => true,
         ));
 
         $this->addElement('text', 'destination_field', array(
-            'label' => $this->translate('Destination Field'),
-            'required'    => true,
+            'label'    => $this->translate('Destination Field'),
+            'required' => true,
         ));
 
         $this->addElement('text', 'priority', array(
-            'label' => $this->translate('Priority'),
+            'label'       => $this->translate('Priority'),
             'description' => $this->translate('Priority for the specified source expression'),
             'required'    => true,
         ));
 
         $this->addElement('text', 'filter_expression', array(
-            'label' => $this->translate('Filter Expression'),
+            'label'       => $this->translate('Filter Expression'),
             'description' => $this->translate('This allows to filter for specific parts within the given source expression'),
             'required'    => false,
         ));
     
         $this->addElement('select', 'merge_policy', array(
-            'label'       => $this->translate('Merge Policy'),
-	     'description' => $this->translate('Whether you want to merge or override the destination field'),
-            'required'    => true,
+            'label'        => $this->translate('Merge Policy'),
+            'description'  => $this->translate('Whether you want to merge or override the destination field'),
+            'required'     => true,
             'multiOptions' => array(
-            'null'     => '- please choose -',
-            'merge'       => 'merge',
-            'override' => 'override'
+            'null'         => '- please choose -',
+            'merge'        => 'merge',
+            'override'     => 'override'
         )
         ));
 
@@ -59,16 +59,6 @@ class SyncPropertyForm extends DirectorObjectForm
         
         parent::loadObject($id);
         return $this;
-    }
-
-    public function onSuccess()
-    {
-/*
-        $this->getElement('owner')->setValue(
-            self::username()
-        );
-*/
-        parent::onSuccess();
     }
 
     public function setDb($db) 
