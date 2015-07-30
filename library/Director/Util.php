24cd6499087c347f1df34cf4ad99b571479ea2ce
Thomas Gelf <thomas@gelf.net>
2015-07-30 09:12:19 +0200

    Util: add missing use, remove backslash

--- library/Director/Util.php
+++ library/Director/Util.php
@@ -5,6 +5,7 @@ namespace Icinga\Module\Director;
 use Icinga\Authentication\Manager;
 use Icinga\Data\ResourceFactory;
 use Icinga\Module\Director\Web\Form\QuickForm;
+use Icinga\Web\Url;
 use Zend_Db_Expr;
 
 class Util
@@ -15,7 +16,7 @@ class Util
 
     public static function pgBinEscape($binary)
     {
-        return new \Zend_Db_Expr("'\\x" . bin2hex($binary) . "'");
+        return new Zend_Db_Expr("'\\x" . bin2hex($binary) . "'");
     }
 
     public static function hex2binary($bin)
