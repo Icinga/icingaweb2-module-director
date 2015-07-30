0bc2fbe9c7e27029c087d468c33840144a9fb4f2
Thomas Gelf <thomas@gelf.net>
2015-07-30 09:04:42 +0200

    Util: add auth helper methods

--- library/Director/Util.php
+++ library/Director/Util.php
@@ -2,10 +2,13 @@
 
 namespace Icinga\Module\Director;
 
+use Icinga\Authentication\Manager;
 use Zend_Db_Expr;
 
 class Util
 {
+    protected static $auth;
+
     public static function pgBinEscape($binary)
     {
         return new \Zend_Db_Expr("'\\x" . bin2hex($binary) . "'");
@@ -19,5 +22,23 @@ class Util
     public static function binary2hex($hex)
     {
         return current(unpack('H*', $hex));
+    }
+
+    public static function auth()
+    {
+        if (self::$auth === null) {
+            self::$auth = Manager::getInstance();
+        }
+        return self::$auth;
+    }
+
+    public static function hasPermission($name)
+    {
+        return self::auth()->hasPermission($name);
+    }
+
+    public static function getRestrictions($name)
+    {
+        return self::auth()->getRestrictions($name);
     }
 }
