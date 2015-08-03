9771e04ee0dca05808779d6f840c9f5e7d77c2cd
Thomas Gelf <thomas@gelf.net>
2015-08-03 13:53:58 +0200

    IcingaCommandArgument: adjust schema column name

--- library/Director/Objects/IcingaCommandArgument.php
+++ library/Director/Objects/IcingaCommandArgument.php
@@ -9,18 +9,18 @@ class IcingaCommandArgument extends IcingaObject
     protected $table = 'icinga_command_argument';
 
     protected $defaultProperties = array(
-        'id'             => null,
-        'command_id'     => null,
-        'argument_name'  => null,
-        'argument_value' => null,
-        'key_string'     => null,
-        'description'    => null,
-        'skip_key'       => null,
-        'set_if'         => null,
-        'sort_order'     => null,
-        'repeat_key'     => null,
-        'value_format'   => null,
-        'set_if_format'  => null,
+        'id'              => null,
+        'command_id'      => null,
+        'argument_name'   => null,
+        'argument_value'  => null,
+        'argument_format' => null,
+        'key_string'      => null,
+        'description'     => null,
+        'skip_key'        => null,
+        'set_if'          => null,
+        'sort_order'      => null,
+        'repeat_key'      => null,
+        'set_if_format'   => null,
     );
 
     public function onInsert()
