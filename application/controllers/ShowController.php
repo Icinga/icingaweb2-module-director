632873595baef1de80d678572d34b6e40cd13e00
Thomas Gelf <thomas@gelf.net>
2015-08-28 23:45:16 +0200

    show/activitylog: show object syntax, not props

--- application/controllers/ShowController.php
+++ application/controllers/ShowController.php
@@ -1,6 +1,7 @@
 <?php
 
 use Icinga\Module\Director\IcingaConfig\IcingaConfig;
+use Icinga\Module\Director\Objects\IcingaObject;
 use Icinga\Module\Director\Util;
 use Icinga\Module\Director\Web\Controller\ActionController;
 
@@ -14,6 +15,30 @@ class Director_ShowController extends ActionController
             $this->view->entry = $this->db()->fetchActivityLogEntry(Util::hex2binary($checksum));
         }
 
+        $entry = $this->view->entry;
+
+        if ($entry->old_properties) {
+            $old = $this->createObject($entry->object_type, $entry->old_properties);
+            if ($entry->new_properties) {
+                $new = $this->createObject($entry->object_type, $entry->old_properties);
+                $new->setProperties((array) json_decode($entry->new_properties));
+            } else {
+                $new = null;
+            }
+        } else {
+            $old = null;
+            $new = $this->createObject($entry->object_type, $entry->new_properties);
+        }
+
+        $this->view->entry = $entry;
+        $this->view->newObject = $new;
+        $this->view->oldObject = $old;
         $this->view->title = $this->translate('Activity');
+    }
+
+    protected function createObject($type, $props)
+    {
+        $props = json_decode($props);
+        return IcingaObject::createByType($type, (array) $props, $this->db());
     }
 }
