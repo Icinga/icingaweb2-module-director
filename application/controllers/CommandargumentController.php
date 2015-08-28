b427ffe6399664b7562272ade330c2b623835e7b
Thomas Gelf <thomas@gelf.net>
2015-08-28 17:59:46 +0200

    Commandargument(s)Controller: remove, obsolete

--- application/controllers/CommandargumentController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Icinga\Module\Director\Web\Controller\ObjectController;
-
-class Director_CommandargumentController extends ObjectController
-{
-}
--- application/controllers/CommandargumentsController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Icinga\Module\Director\Web\Controller\ObjectsController;
-
-class Director_CommandargumentsController extends ObjectsController
-{
-}
