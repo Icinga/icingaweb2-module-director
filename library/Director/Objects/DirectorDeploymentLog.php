ce0a9828216d2086b7d834c29d0e0a37874aee19
Thomas Gelf <thomas@gelf.net>
2015-08-29 01:07:26 +0200

    DirectorDeploymentLog: new object

---
+++ library/Director/Objects/DirectorDeploymentLog.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Icinga\Module\Director\Objects;
+
+use Icinga\Module\Director\Data\Db\DbObject;
+
+class DirectorDeploymentLog extends DbObject
+{
+    protected $table = 'director_deployment_log';
+
+    protected $keyName = 'id';
+
+    protected $autoincKeyName = 'id';
+
+    protected $defaultProperties = array(
+        'id'                   => null,
+        'config_id'            => null,
+        'peer_identity'        => null,
+        'start_time'           => null,
+        'end_time'             => null,
+        'abort_time'           => null,
+        'duration_connection'  => null,
+        'duration_dump'        => null,
+        'stage_name'           => null,
+        'stage_collected'      => null,
+        'connection_succeeded' => null,
+        'dump_succeeded'       => null,
+        'startup_succeeded'    => null,
+        'username'             => null,
+        'startup_log'          => null,
+    );
+}
