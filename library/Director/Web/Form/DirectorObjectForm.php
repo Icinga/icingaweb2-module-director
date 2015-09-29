386a7ee90d77282069bb7c6c3533902cb026ecd5
Thomas Gelf <thomas@gelf.net>
2015-09-29 18:47:49 +0200

    DirectorObjectForm: gracefully handle missing...
    ...data type implementations. Happens when fields have been configured
    with data types provided by modules that have been disabled later on.

--- library/Director/Web/Form/DirectorObjectForm.php
+++ library/Director/Web/Form/DirectorObjectForm.php
@@ -271,10 +271,18 @@ abstract class DirectorObjectForm extends QuickForm
     protected function addField($field, $value = null, $inherited = null, $inheritedFrom = null)
     {
         $datafield = DirectorDatafield::load($field->datafield_id, $this->getDb());
-        $datatype = new $datafield->datatype;
-        $datatype->setSettings($datafield->getSettings());
-
         $name = 'var_' . $datafield->varname;
+        $className = $datafield->datatype;
+
+        if (! class_exists($className)) {
+            $this->addElement('text', $name, array('disabled' => 'disabled'));
+            $el = $this->getElement($name);
+            $el->addError(sprintf('Form element could not be created, %s is missing', $className));
+            return $el;
+        }
+
+        $datatype = new $className;
+        $datatype->setSettings($datafield->getSettings());
         $el = $datatype->getFormElement($name, $this);
 
         $el->setLabel($datafield->caption);
