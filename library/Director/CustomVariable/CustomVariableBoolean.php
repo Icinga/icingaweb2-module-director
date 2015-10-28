51813557c9591b3835be0f13215a20c5b6b93190
Thomas Gelf <thomas@gelf.net>
2015-10-28 22:27:01 +0100

    CustomVariableBoolean: add a new custom var type

---
+++ library/Director/CustomVariable/CustomVariableBoolean.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Icinga\Module\Director\CustomVariable;
+
+use Icinga\Exception\ProgrammingError;
+
+class CustomVariableBoolean extends CustomVariable
+{
+    public function equals(CustomVariable $var)
+    {
+        return $var->getValue() === $this->getValue();
+    }
+
+    public function getDbFormat()
+    {
+        return 'json';
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public function setValue($value)
+    {
+        if (! is_bool($value)) {
+            throw new ProgrammingError(
+                'Expected a boolean, got %s',
+                var_export($value, 1)
+            );
+        }
+
+        $this->value = $value;
+
+        return $this;
+    } 
+
+    public function toConfigString()
+    {
+        return $this->value ? 'true' : 'false';
+    }
+}
