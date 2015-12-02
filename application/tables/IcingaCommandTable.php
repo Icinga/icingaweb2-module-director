502e139516083d3289504bdf43acc725a42742ae
Thomas Gelf <thomas@gelf.net>
2015-12-02 03:17:59 +0100

    IcingaCommandTable: one table for all object types

--- application/tables/IcingaCommandTable.php
+++ application/tables/IcingaCommandTable.php
@@ -15,14 +15,35 @@ class IcingaCommandTable extends QuickTable
         return array(
             'id'           => 'c.id',
             'command'      => 'c.object_name',
+            'object_type'  => 'c.object_type',
             'command_line' => 'c.command',
-            'zone'         => 'z.object_name',
         );
     }
 
     protected function getActionUrl($row)
     {
-        return $this->url('director/command', array('name' => $row->command));
+        if ($row->object_type === 'external_object') {
+            return $this->url('director/command/render', array('name' => $row->command));
+        } else {
+            return $this->url('director/command', array('name' => $row->command));
+        }
+    }
+
+    protected function listTableClasses()
+    {
+        return array_merge(array('check-commands'), parent::listTableClasses());
+    }
+
+    protected function getRowClasses($row)
+    {
+        switch ($row->object_type) {
+            case 'object':
+                return 'icinga-object';
+            case 'template':
+                return 'icinga-template';
+            case 'external_object':
+                return 'icinga-object-external';
+        }
     }
 
     public function getTitles()
@@ -40,17 +61,13 @@ class IcingaCommandTable extends QuickTable
         $query = $db->select()->from(
             array('c' => 'icinga_command'),
             array()
-        )->joinLeft(
-            array('z' => 'icinga_zone'),
-            'c.zone_id = z.id',
-            array()
-        );
+        )->order('c.object_name');
 
         return $query;
     }
 
     public function getBaseQuery()
     {
-        return $this->getUnfilteredQuery()->where('c.object_type = ?', 'object');
+        return $this->getUnfilteredQuery();
     }
 }
