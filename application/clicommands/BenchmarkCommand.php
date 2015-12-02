b8f246b11dda2e535ee81c292528456f5ae68844
Thomas Gelf <thomas@gelf.net>
2015-12-02 04:04:38 +0100

    cli: add benchmark command, for userspace filters

---
+++ application/clicommands/BenchmarkCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Icinga\Module\Director\Clicommands;
+
+use Icinga\Application\Benchmark;
+use Icinga\Data\Filter\Filter;
+use Icinga\Module\Director\Cli\Command;
+use Icinga\Module\Director\Objects\IcingaHost;
+use Icinga\Module\Director\Objects\IcingaHostVar;
+
+class BenchmarkCommand extends Command
+{
+    public function filterAction()
+    {
+        $flat = array();
+
+        $filter = Filter::fromQueryString(
+//            'object_name=*ic*2*&object_type=object'
+'vars.bpconfig=*'
+        );
+        Benchmark::measure('ready');
+        $objs = IcingaHost::loadAll($this->db());
+        Benchmark::measure('db done');
+
+        foreach ($objs as $host) {
+            $flat[$host->id] = (object) array();
+            foreach ($host->getProperties() as $k => $v) {
+                $flat[$host->id]->$k = $v;
+            }
+        }
+        Benchmark::measure('objects ready');
+
+        $vars = IcingaHostVar::loadAll($this->db());
+        Benchmark::measure('vars loaded');
+        foreach ($vars as $var) {
+            if (! array_key_exists($var->host_id, $flat)) continue; // Templates?
+            $flat[$var->host_id]->{'vars.' . $var->varname} = $var->varvalue;
+        }
+        Benchmark::measure('vars done');
+
+        foreach ($flat as $host) {
+            if ($filter->matches($host)) {
+                echo $host->object_name . "\n";
+            }
+        }
+        return;
+
+        Benchmark::measure('all done');
+    }
+}
