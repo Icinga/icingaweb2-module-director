3e577ce65b41a9fb4d3ed02e41d9045087f6e7aa
Thomas Gelf <thomas@gelf.net>
2015-12-10 13:00:08 +0100

    HostController: add services tab

--- application/controllers/HostController.php
+++ application/controllers/HostController.php
@@ -6,4 +6,23 @@ use Icinga\Module\Director\Web\Controller\ObjectController;
 
 class HostController extends ObjectController
 {
+    public function init()
+    {
+        parent::init();
+        if ($this->object) {
+            $this->getTabs()->add('services', array(
+                'url'       => 'director/host/services',
+                'urlParams' => array('name' => $this->object->object_name),
+                'label'     => 'Services'
+            ));
+        }
+    }
+
+    public function servicesAction()
+    {
+        $this->getTabs()->activate('services');
+        $this->view->title = $this->translate('Services');
+        $this->view->table = $this->loadTable('IcingaService')->enforceFilter('host_id', $this->object->id)->setConnection($this->db());
+        $this->render('objects/table', null, true);
+    }
 }
