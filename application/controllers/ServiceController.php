0f9f172ebb9e7cb52d26bcd28c782a7b7a7d0683
Thomas Gelf <thomas@gelf.net>
2015-12-10 12:52:10 +0100

    ServiceController: support the host parameter

--- application/controllers/ServiceController.php
+++ application/controllers/ServiceController.php
@@ -3,6 +3,8 @@
 namespace Icinga\Module\Director\Controllers;
 
 use Icinga\Module\Director\Web\Controller\ObjectController;
+use Icinga\Module\Director\Objects\IcingaService;
+use Icinga\Module\Director\Objects\IcingaHost;
 
 class ServiceController extends ObjectController
 {
@@ -15,6 +17,12 @@ class ServiceController extends ObjectController
                 'urlParams' => array('name' => $this->object->object_name),
                 'label' => 'Assign'
             ));
+
+            if ($host = $this->params->get('host')) {
+                foreach ($this->getTabs()->getTabs() as $tab) {
+                    $tab->getUrl()->setParam('host', $host);
+                }
+            }
         }
     }
 
@@ -31,5 +39,22 @@ class ServiceController extends ObjectController
             ->setObject($this->object);
         $this->view->title = 'Assign service to host';
         $this->render('object/fields', null, true); // TODO: render table
+    }
+
+    protected function loadObject()
+    {
+        if ($this->object === null && $name = $this->params->get('name')) {
+            $params = array('object_name' => $name);
+            $db = $this->db();
+
+            if ($hostname = $this->params->get('host')) {
+                $this->view->host = IcingaHost::load($hostname, $db);
+                $params['host_id'] = $this->view->host->id;
+            }
+
+            $this->object = IcingaService::load($params, $db);
+        }
+
+        return $this->object;
     }
 }
