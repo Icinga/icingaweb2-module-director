c6557d6d6ca4c876d26ceaa18248d368a47bb788
Thomas Gelf <thomas@gelf.net>
2015-12-15 19:32:12 +0100

    IcingaEndpointForm: disable object type

--- application/forms/IcingaEndpointForm.php
+++ application/forms/IcingaEndpointForm.php
@@ -9,7 +9,7 @@ class IcingaEndpointForm extends DirectorObjectForm
     public function setup()
     {
         $isTemplate = $this->getSentValue('object_type') === 'template';
-
+/*
         $this->addElement('select', 'object_type', array(
             'label' => $this->translate('Object type'),
             'description' => $this->translate('Whether this should be a template'),
@@ -19,7 +19,7 @@ class IcingaEndpointForm extends DirectorObjectForm
                 'template' => 'Endpoint template',
             )
         ));
-
+*/
         if ($isTemplate) {
             $this->addElement('text', 'object_name', array(
                 'label'       => $this->translate('Endpoint template name'),
