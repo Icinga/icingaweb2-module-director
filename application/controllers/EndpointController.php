017f4b09d1fe43ef8868674b75fa289aa3d741c4
Thomas Gelf <thomas@gelf.net>
2016-02-17 16:47:03 +0100

    EndpointController: remove peer detection

--- application/controllers/EndpointController.php
+++ application/controllers/EndpointController.php
@@ -9,7 +9,7 @@ class EndpointController extends ObjectController
     public function init()
     {
         parent::init();
-        if ($this->isPeer()) {
+        if ($this->object && $this->object->hasApiUser()) {
             $params['endpoint'] = $this->object->object_name;
 
             $this->getTabs()->add('inspect', array(
@@ -17,21 +17,6 @@ class EndpointController extends ObjectController
                 'urlParams' => $params,
                 'label'     => $this->translate('Inspect')
             ));
-        }
-    }
-
-    protected function isPeer()
-    {
-        if (! $this->object) return false;
-
-        $apiconfig = $this->Config()->getSection('api');
-        $host = $apiconfig->get('address');
-        $port = $apiconfig->get('port');
-
-        if ($host === $this->object->host
-            && $port === $this->object->port
-        ) {
-            return true;
         }
     }
 }
