3df01158955a22fec3dcdc86f8325cc1434df19f
Thomas Gelf <thomas@gelf.net>
2016-02-17 16:41:46 +0100

    run.php: cleanup, add property modifiers

--- run.php
+++ run.php
@@ -1,14 +1,24 @@
 <?php
 
+$prefix = '\\Icinga\\Module\\Director\\';
+
 $this->provideHook('monitoring/HostActions');
 $this->provideHook('monitoring/ServiceActions');
 
-$this->provideHook('director/ImportSource', '\\Icinga\\Module\\Director\\Import\\ImportSourceSql');
-$this->provideHook('director/ImportSource', '\\Icinga\\Module\\Director\\Import\\ImportSourceLdap');
-$this->provideHook('director/ImportSource', '\\Icinga\\Module\\Director\\Import\\ImportSourceCoreApi');
+$this->provideHook('director/ImportSource', $prefix . 'Import\\ImportSourceSql');
+$this->provideHook('director/ImportSource', $prefix . 'Import\\ImportSourceLdap');
+$this->provideHook('director/ImportSource', $prefix . 'Import\\ImportSourceCoreApi');
 
-$this->provideHook('director/DataType', '\\Icinga\\Module\\Director\\DataType\\DataTypeString');
-$this->provideHook('director/DataType', '\\Icinga\\Module\\Director\\DataType\\DataTypeNumber');
-$this->provideHook('director/DataType', '\\Icinga\\Module\\Director\\DataType\\DataTypeTime');
-$this->provideHook('director/DataType', '\\Icinga\\Module\\Director\\DataType\\DataTypeDatalist');
-$this->provideHook('director/DataType', '\\Icinga\\Module\\Director\\DataType\\DataTypeSqlQuery');
+$this->provideHook('director/DataType', $prefix . 'DataType\\DataTypeString');
+$this->provideHook('director/DataType', $prefix . 'DataType\\DataTypeNumber');
+$this->provideHook('director/DataType', $prefix . 'DataType\\DataTypeTime');
+$this->provideHook('director/DataType', $prefix . 'DataType\\DataTypeDatalist');
+$this->provideHook('director/DataType', $prefix . 'DataType\\DataTypeSqlQuery');
+
+$this->provideHook('director/PropertyModifier', $prefix . 'DataType\\PropertyModifierLowercase');
+$this->provideHook('director/PropertyModifier', $prefix . 'DataType\\PropertyModifierRegexReplace');
+$this->provideHook('director/PropertyModifier', $prefix . 'DataType\\PropertyModifierReplace');
+$this->provideHook('director/PropertyModifier', $prefix . 'DataType\\PropertyModifierStripDomain');
+$this->provideHook('director/PropertyModifier', $prefix . 'DataType\\PropertyModifierSubstring');
+$this->provideHook('director/PropertyModifier', $prefix . 'DataType\\PropertyModifierUppercase');
+
