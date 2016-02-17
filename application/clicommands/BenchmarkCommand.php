b6a8ce7acc99984e354e7053cdf3667ab353a84b
Thomas Gelf <thomas@gelf.net>
2016-02-17 16:44:23 +0100

    BenchmarkCommand: remove api instantiation logic

--- application/clicommands/BenchmarkCommand.php
+++ application/clicommands/BenchmarkCommand.php
@@ -8,11 +8,6 @@ use Icinga\Module\Director\Cli\Command;
 use Icinga\Module\Director\Objects\IcingaHost;
 use Icinga\Module\Director\Objects\IcingaHostVar;
 
-
-use Icinga\Module\Director\Core\RestApiClient;
-use Icinga\Module\Director\Core\CoreApi;
-
-
 class BenchmarkCommand extends Command
 {
     public function streamAction()
@@ -57,16 +52,4 @@ class BenchmarkCommand extends Command
 
         Benchmark::measure('all done');
     }
-
-
-
-    protected function api()
-    {
-        $apiconfig = $this->Config()->getSection('api');
-        $client = new RestApiClient($apiconfig->get('address'), $apiconfig->get('port'));
-        $client->setCredentials($apiconfig->get('username'), $apiconfig->get('password'));
-        $api = new CoreApi($client);
-        return $api;
-    }
-
 }
