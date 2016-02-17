402b4c31e26bfc4ef1ecb438c2f4d5f2020a8ba0
Thomas Gelf <thomas@gelf.net>
2016-02-17 16:39:18 +0100

    IcingaEndpoint: add api user and client helper

--- library/Director/Objects/IcingaEndpoint.php
+++ library/Director/Objects/IcingaEndpoint.php
@@ -2,6 +2,9 @@
 
 namespace Icinga\Module\Director\Objects;
 
+use Icinga\Module\Director\Core\CoreApi;
+use Icinga\Module\Director\Core\RestApiClient;
+
 class IcingaEndpoint extends IcingaObject
 {
     protected $table = 'icinga_endpoint';
@@ -24,6 +27,36 @@ class IcingaEndpoint extends IcingaObject
         'zone'    => 'IcingaZone',
         'apiuser' => 'IcingaApiUser',
     );
+
+    public function hasApiUser()
+    {
+        return $this->getResolvedProperty('apiuser_id') !== null;
+    }
+
+    public function getApiUser()
+    {
+        return $this->getRelatedObject(
+            'apiuser',
+            $this->getResolvedProperty('apiuser_id')
+        );
+    }
+
+    public function api()
+    {
+        $client = new RestApiClient(
+            $this->getResolvedProperty('host'),
+            $this->getResolvedProperty('port')
+        );
+
+        $user = $this->getApiUser();
+        $client->setCredentials(
+            // TODO: $user->client_dn,
+            $user->object_name,
+            $user->password
+        );
+
+        return new CoreApi($client);
+    }
 
     protected function renderLog_duration()
     {
