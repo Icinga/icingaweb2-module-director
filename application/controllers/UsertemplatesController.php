0574217568546fbdd7166ca4433d2754ac1f2dc0
Thomas Gelf <thomas@gelf.net>
2016-02-27 01:43:03 +0100

    controllers/*templates: remove obsolete controllers

--- application/controllers/UsertemplatesController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Icinga\Module\Director\Controllers;
-
-use Icinga\Module\Director\Web\Controller\ObjectsController;
-
-class UsertemplatesController extends ObjectsController
-{
-}
