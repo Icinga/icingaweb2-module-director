cef7090666c7ea98528e360c84a40df0f09bbe83
Thomas Gelf <thomas@gelf.net>
2016-03-06 14:22:47 +0100

    IcingaServiceAssignment: new dedicated object type

---
+++ library/Director/Objects/IcingaServiceAssignment.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Icinga\Module\Director\Objects;
+
+class IcingaServiceAssignment extends IcingaObject
+{
+    protected $table = 'icinga_service_assignment';
+
+    protected $defaultProperties = array(
+        'id'            => null,
+        'service_id'    => null,
+        'filter_string' => null,
+    );
+
+    protected $relations = array(
+        'service' => 'IcingaService',
+    );
+}
