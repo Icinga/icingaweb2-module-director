af6225a74ef46dbffd989e0ff79a27556e8dfebb
Thomas Gelf <thomas@gelf.net>
2016-03-14 12:39:41 +0100

    ImportSourceForm: add more descriptions

--- application/forms/ImportSourceForm.php
+++ application/forms/ImportSourceForm.php
@@ -10,15 +10,25 @@ class ImportSourceForm extends DirectorObjectForm
     public function setup()
     {
         $this->addElement('text', 'source_name', array(
-            'label' => $this->translate('Import source name'),
+            'label'       => $this->translate('Import source name'),
+            'description' => $this->translate(
+                'A short name identifying this import source. Use something meaningful,'
+                . ' like "Hosts from Puppet", "Users from Active Directory" or similar'
+            ),
             'required'    => true,
         ));
 
         $this->addElement('select', 'provider_class', array(
-            'label'       => $this->translate('Source Type'),
-            'required'    => true,
+            'label'        => $this->translate('Source Type'),
+            'required'     => true,
             'multiOptions' => $this->optionalEnum($this->enumSourceTypes()),
-            'class'       => 'autosubmit'
+            'description'  => $this->translate(
+                'These are different data providers fetching data from various sources.'
+                . ' You didn\t find what you\'re looking for? Import sources are implemented'
+                . ' as a hook in Director, so you might find (or write your own) Icinga Web 2'
+                . ' module fetching data from wherever you want'
+            ),
+            'class'        => 'autosubmit'
         ));
 
         $this->addSettings();
@@ -51,7 +61,13 @@ class ImportSourceForm extends DirectorObjectForm
 
         $this->addElement('text', 'key_column', array(
             'label' => $this->translate('Key column name'),
-            'description' => $this->translate('This must be a column containing unique values like hostnames'),
+            'description' => $this->translate(
+                'This must be a column containing unique values like hostnames. Unless otherwise'
+                . ' specified this will then be used as the object_name for the syncronized'
+                . ' Icinga object. Especially when getting started with director please make'
+                . ' sure to strictly follow this rule. Duplicate values for this column on different'
+                . ' rows will trigger a failure, your import run will not succeed'
+            ),
             'placeholder' => $defaultKeyCol,
             'required'    => $defaultKeyCol === null,
         ));
