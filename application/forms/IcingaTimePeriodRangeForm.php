cce1e968547fe876b44e6c58e612cf392b4d5fc5
Thomas Gelf <thomas@gelf.net>
2016-03-18 13:44:10 +0100

    IcingaTimePeriodRangeForm: store period, not range

--- application/forms/IcingaTimePeriodRangeForm.php
+++ application/forms/IcingaTimePeriodRangeForm.php
@@ -38,4 +38,44 @@ class IcingaTimePeriodRangeForm extends DirectorObjectForm
         $this->period = $period;
         return $this;
     }
+
+    public function onSuccess()
+    {
+        $object = $this->object();
+        if ($object->hasBeenModified()) {
+            $this->period->ranges()->setRange(
+                $this->getValue('timeperiod_key'),
+                $this->getValue('timeperiod_value')
+            );
+        }
+
+        if ($this->period->hasBeenModified()) {
+            if (! $object->hasBeenLoadedFromDb()) {
+
+                $this->setHttpResponseCode(201);
+            }
+            $msg = sprintf(
+                $object->hasBeenLoadedFromDb()
+                ? $this->translate('The %s has successfully been stored')
+                : $this->translate('A new %s has successfully been created'),
+                $this->translate($this->getObjectName())
+            );
+
+            $this->period->store($this->db);
+
+        } else {
+            if ($this->isApiRequest()) {
+                $this->setHttpResponseCode(304);
+            }
+            $msg = $this->translate('No action taken, object has not been modified');
+        }
+        if ($object instanceof IcingaObject) {
+            $this->setSuccessUrl(
+                'director/' . strtolower($this->getObjectName()),
+                $object->getUrlParams()
+            );
+        }
+
+        $this->redirectOnSuccess($msg);
+    }
 }
