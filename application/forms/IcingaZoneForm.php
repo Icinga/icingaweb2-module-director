42b668d4ae3cffdce6dffcbdfef01f4bc023d78f
Thomas Gelf <thomas@gelf.net>
2016-03-18 15:30:22 +0100

    IcingaZoneForm: improve descriptions

--- application/forms/IcingaZoneForm.php
+++ application/forms/IcingaZoneForm.php
@@ -11,14 +11,20 @@ class IcingaZoneForm extends DirectorObjectForm
         $this->addHidden('object_type', 'object');
 
         $this->addElement('text', 'object_name', array(
-            'label'       => $this->translate('Zone (template) name'),
+            'label'       => $this->translate('Zone name'),
             'required'    => true,
-            'description' => $this->translate('Name for the Icinga zone (templat) you are going to create')
+            'description' => $this->translate(
+                'Name for the Icinga zone you are going to create'
+            )
         ));
 
         $this->addElement('select', 'is_global', array(
-            'label'        => 'Global zone',
-            'description'  => 'Whether this zone should be available everywhere',
+            'label'        => $this->translate('Global zone'),
+            'description'  => $this->translate(
+                'Whether this zone should be available everywhere. Please note that'
+                . ' it rarely leads to the desired result when you try to distribute'
+                . ' global zones in distrubuted environments'
+            ),
             'multiOptions' => array(
                 'n'  => $this->translate('No'),
                 'y'  => $this->translate('Yes'),
@@ -32,7 +38,6 @@ class IcingaZoneForm extends DirectorObjectForm
             'multiOptions' => $this->optionalEnum($this->db->enumZones()),
         ));
 
-        // $this->addImportsElement();
         $this->setButtons();
     }
 }
