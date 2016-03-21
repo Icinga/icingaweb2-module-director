5e540880e54d8acb7747350117abde1e83df776b
Thomas Gelf <thomas@gelf.net>
2016-03-21 22:51:03 +0100

    Revert "DirectorObjectForm: do not set null defaults"
    This reverts commit ce7b6031ff6964ea53c2b1d45039c6849cfd5fe7.
    
    Broke quite some forms

--- library/Director/Web/Form/DirectorObjectForm.php
+++ library/Director/Web/Form/DirectorObjectForm.php
@@ -183,18 +183,10 @@ abstract class DirectorObjectForm extends QuickForm
             unset($props['vars']);
         }
 
-        $null = array();
         foreach ($props as $k => $v) {
             if (is_bool($v)) {
                 $props[$k] = $v ? 'y' : 'n';
             }
-            if ($v === null) {
-                $null[] = $k;
-            }
-        }
-
-        foreach ($null as $k) {
-            unset($props[$k]);
         }
 
         $this->setDefaults($props);
