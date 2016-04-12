97e2a73a4880e771a46488ee397f06f943f2b763
Thomas Gelf <thomas@gelf.net>
2016-04-12 20:32:30 +0200

    SyncRuleForm: refine descriptions

--- application/forms/SyncRuleForm.php
+++ application/forms/SyncRuleForm.php
@@ -30,35 +30,50 @@ class SyncRuleForm extends DirectorObjectForm
 
         $this->addElement('select', 'object_type', array(
             'label'        => $this->translate('Object Type'),
-            'description'  => $this->translate('Choose a object type'),
+            'description'  => $this->translate('Choose an object type'),
             'required'     => true,
             'multiOptions' => $this->optionalEnum($availableTypes)
         ));
 
         $this->addElement('select', 'update_policy', array(
             'label'        => $this->translate('Update Policity'),
-            'description'  => $this->translate('Whether the field should be merged, replaced or ignored'),
+            'description'  => $this->translate(
+                'Define what should happen when an object with a matching key'
+              . " already exists. You could merge it's properties (import source"
+              . ' wins), replace it completely with the imported object or ignore'
+              . ' it (helpful for one-time imports)'
+            ),
             'required'     => true,
             'multiOptions' => $this->optionalEnum(array(
-                'merge'    => 'merge',
-                'override' => 'replace',
-                'ignore'   => 'ignore'
+                'merge'    => $this->translate('Merge'),
+                'override' => $this->translate('Replace'),
+                'ignore'   => $this->translate('Ignore'),
             ))
         ));
 
         $this->addElement('select', 'purge_existing', array(
             'label' => $this->translate('Purge'),
-            'description' => $this->translate('Purge existing values.'),
-            'required'    => true,
+            'description'  => $this->translate(
+                'Whether to purge existing objects. This means that objects of'
+              . ' the same type will be removed from Director in case they no'
+              . ' longer exist at your import source.'
+            ),
+            'required'     => true,
             'multiOptions' => $this->optionalEnum(array(
-                'y' => 'yes',
-                'n' => 'no'
+                'y' => $this->translate('Yes'),
+                'n' => $this->translate('No')
             ))
         ));
 
         $this->addElement('text', 'filter_expression', array(
             'label'       => $this->translate('Filter Expression'),
-            'description' => $this->translate('This allows to filter for specific parts'),
+            'description' => sprintf(
+                $this->translate(
+                    'Sync only part of your imported objects with this rule. Icinga Web 2'
+                  . ' filter syntax is allowed, so this could look as follows: %s'
+                ),
+                '(host=a|host=b)&!ip=127.*'
+            ),
         ));
 
         $this->setButtons();
