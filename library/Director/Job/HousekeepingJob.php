68d7f9098c51a65e5477c1f73a64e6dd240c910c
Thomas Gelf <thomas@gelf.net>
2016-04-22 10:04:48 +0200

    HousekeepingJob: add description

--- library/Director/Job/HousekeepingJob.php
+++ library/Director/Job/HousekeepingJob.php
@@ -4,6 +4,7 @@ namespace Icinga\Module\Director\Job;
 
 use Icinga\Module\Director\Db\Housekeeping;
 use Icinga\Module\Director\Hook\JobHook;
+use Icinga\Module\Director\Web\Form\QuickForm;
 
 class HousekeepingJob extends JobHook
 {
@@ -12,6 +13,14 @@ class HousekeepingJob extends JobHook
     public function run()
     {
         $this->housekeeping()->runAllTasks();
+    }
+
+    public static function getDescription(QuickForm $form)
+    {
+        return $form->translate(
+            'The Housekeeping job provides various task that keep your Director'
+            . ' database fast and clean'
+        );
     }
 
     public function isPending()
