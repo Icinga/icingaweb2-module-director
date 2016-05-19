a8808d53c0e02c07bca7f941f406def128bb8540
Thomas Gelf <thomas@gelf.net>
2016-05-19 15:04:10 +0200

    run.php: sort data types

--- run.php
+++ run.php
@@ -11,13 +11,12 @@ $this->provideHook('director/ImportSource', $prefix . 'Import\\ImportSourceSql')
 $this->provideHook('director/ImportSource', $prefix . 'Import\\ImportSourceLdap');
 $this->provideHook('director/ImportSource', $prefix . 'Import\\ImportSourceCoreApi');
 
-$this->provideHook('director/DataType', $prefix . 'DataType\\DataTypeString');
 $this->provideHook('director/DataType', $prefix . 'DataType\\DataTypeArray');
-$this->provideHook('director/DataType', $prefix . 'DataType\\DataTypeNumber');
-$this->provideHook('director/DataType', $prefix . 'DataType\\DataTypeTime');
 $this->provideHook('director/DataType', $prefix . 'DataType\\DataTypeBoolean');
 $this->provideHook('director/DataType', $prefix . 'DataType\\DataTypeDatalist');
+$this->provideHook('director/DataType', $prefix . 'DataType\\DataTypeNumber');
 $this->provideHook('director/DataType', $prefix . 'DataType\\DataTypeSqlQuery');
+$this->provideHook('director/DataType', $prefix . 'DataType\\DataTypeString');
 
 $this->provideHook('director/PropertyModifier', $prefix . 'PropertyModifier\\PropertyModifierLowercase');
 $this->provideHook('director/PropertyModifier', $prefix . 'PropertyModifier\\PropertyModifierRegexReplace');
