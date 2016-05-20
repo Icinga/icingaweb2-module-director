44d9392e708a9d80649d03f44dc00c63116f0371
Thomas Gelf <thomas@gelf.net>
2016-05-21 00:27:45 +0200

    schema/mysql.sql: fix ordering

--- schema/mysql.sql
+++ schema/mysql.sql
@@ -140,38 +140,6 @@ CREATE TABLE director_datafield_setting (
     ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE director_job (
-  id INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
-  job_name VARCHAR(64) NOT NULL,
-  job_class VARCHAR(72) NOT NULL,
-  disabled ENUM('y', 'n') NOT NULL DEFAULT 'n',
-  run_interval INT(10) UNSIGNED NOT NULL, -- seconds
-  timeperiod_id INT(10) UNSIGNED DEFAULT NULL,
-  last_attempt_succeeded ENUM('y', 'n') DEFAULT NULL,
-  ts_last_attempt DATETIME DEFAULT NULL,
-  ts_last_error DATETIME DEFAULT NULL,
-  last_error_message TEXT,
-  PRIMARY KEY (id),
-  UNIQUE KEY (job_name),
-  CONSTRAINT director_job_period
-    FOREIGN KEY timeperiod (timeperiod_id)
-    REFERENCES icinga_timeperiod (id)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE director_job_setting (
-  job_id INT UNSIGNED NOT NULL,
-  setting_name VARCHAR(64) NOT NULL,
-  setting_value TEXT DEFAULT NULL,
-  PRIMARY KEY (job_id, setting_name),
-  CONSTRAINT job_settings
-    FOREIGN KEY director_job (job_id)
-    REFERENCES director_job (id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 CREATE TABLE director_schema_migration (
   schema_version SMALLINT UNSIGNED NOT NULL,
   migration_time DATETIME NOT NULL,
@@ -265,6 +233,38 @@ CREATE TABLE icinga_timeperiod_range (
   CONSTRAINT icinga_timeperiod_range_timeperiod
     FOREIGN KEY timeperiod (timeperiod_id)
     REFERENCES icinga_timeperiod (id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE director_job (
+  id INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
+  job_name VARCHAR(64) NOT NULL,
+  job_class VARCHAR(72) NOT NULL,
+  disabled ENUM('y', 'n') NOT NULL DEFAULT 'n',
+  run_interval INT(10) UNSIGNED NOT NULL, -- seconds
+  timeperiod_id INT(10) UNSIGNED DEFAULT NULL,
+  last_attempt_succeeded ENUM('y', 'n') DEFAULT NULL,
+  ts_last_attempt DATETIME DEFAULT NULL,
+  ts_last_error DATETIME DEFAULT NULL,
+  last_error_message TEXT,
+  PRIMARY KEY (id),
+  UNIQUE KEY (job_name),
+  CONSTRAINT director_job_period
+    FOREIGN KEY timeperiod (timeperiod_id)
+    REFERENCES icinga_timeperiod (id)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE director_job_setting (
+  job_id INT UNSIGNED NOT NULL,
+  setting_name VARCHAR(64) NOT NULL,
+  setting_value TEXT DEFAULT NULL,
+  PRIMARY KEY (job_id, setting_name),
+  CONSTRAINT job_settings
+    FOREIGN KEY director_job (job_id)
+    REFERENCES director_job (id)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
