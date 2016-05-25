7fea8c47877e823cee119432850392d25d305a88
Thomas Gelf <thomas@gelf.net>
2016-05-25 08:14:39 +0200

    DataTypeNumber: filter and validate numbers

--- library/Director/DataType/DataTypeNumber.php
+++ library/Director/DataType/DataTypeNumber.php
@@ -4,12 +4,15 @@ namespace Icinga\Module\Director\DataType;
 
 use Icinga\Module\Director\Hook\DataTypeHook;
 use Icinga\Module\Director\Web\Form\QuickForm;
+use Icinga\Module\Director\Data\ValueFilter\FilterInt;
 
 class DataTypeNumber extends DataTypeHook
 {
     public function getFormElement($name, QuickForm $form)
     {
-        $element = $form->createElement('text', $name);
+        $element = $form->createElement('text', $name)
+            ->addValidator('int')
+            ->addFilter(new FilterInt);
 
         return $element;
     }
