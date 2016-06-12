04a9a12d846ea12aef905cf8be21bdc17b0f91c6
Thomas Gelf <thomas@gelf.net>
2016-06-12 11:14:12 +0200

    ExtensibleSet: fix phpcs complaint

--- library/Director/Web/Form/Element/ExtensibleSet.php
+++ library/Director/Web/Form/Element/ExtensibleSet.php
@@ -30,8 +30,12 @@ class ExtensibleSet extends FormElement
         return $value;
     }
 
+    /**
+     * @codingStandardsIgnoreStart
+     */
     protected function _filterValue(&$value, &$key)
     {
+        // @codingStandardsIgnoreEnd
         if (is_array($value)) {
             $value = array_filter($value, 'strlen');
         } elseif (is_string($value) && !strlen($value)) {
