bcbe3210630cdf10435914e6ef3ee5437477393a
Thomas Gelf <thomas@gelf.net>
2016-07-13 10:19:51 +0200

    Importrun: add dedicated view script, improve...
    ...controller

--- application/controllers/ImportrunController.php
+++ application/controllers/ImportrunController.php
@@ -2,23 +2,35 @@
 
 namespace Icinga\Module\Director\Controllers;
 
+use Icinga\Module\Director\Objects\ImportRun;
 use Icinga\Module\Director\Web\Controller\ActionController;
 
 class ImportrunController extends ActionController
 {
     public function indexAction()
     {
+        $db = $this->db();
         $id = $this->getRequest()->getUrl()->getParams()->shift('id');
+        $importRun = ImportRun::load($id, $db);
+        $url = clone($this->getRequest()->getUrl());
+        $chosenColumns = $this->getRequest()->getUrl()->shift('chosenColumns');
+
         $this->view->title = $this->translate('Import run');
+        $this->getTabs()->add('importrun', array(
+            'label' => $this->view->title,
+            'url'   => $url
+        ))->activate('importrun');
+
         $table = $this
             ->loadTable('importedrows')
-            ->setConnection($this->db())
-            ->setChecksum(
-                $this->db()->getImportrunRowsetChecksum($id)
-            );
+            ->setConnection($db)
+            ->setImportRun($importRun);
+
+        if ($chosenColumns) {
+            $table->setColumns(preg_split('/,/', $chosenColumns, -1, PREG_SPLIT_NO_EMPTY));
+        }
 
         $this->view->table = $this->applyPaginationLimits($table);
         $this->view->filterEditor = $table->getFilterEditor($this->getRequest());
-        $this->setViewScript('list/table');
     }
 }
