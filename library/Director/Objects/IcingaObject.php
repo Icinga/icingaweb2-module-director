1e540cff1e519faf0602eaa954af4c189079f589
Markus Frosch <markus.frosch@icinga.com>
2016-10-14 13:11:45 +0200

    IcingaObject(Legacy): Render display_name as alias
    With the execption of Host and Service.
    
    refs #12919

--- library/Director/Objects/IcingaObject.php
+++ library/Director/Objects/IcingaObject.php
@@ -1562,6 +1562,20 @@ abstract class IcingaObject extends DbObject implements IcingaConfigRenderer
         return $this->renderLegacyRelationProperty('host', $this->host_id, 'host_name');
     }
 
+    /**
+     * Display Name only exists for host/service in Icinga 1
+     *
+     * Render it as alias for everything by default.
+     *
+     * Alias does not exist in Icinga 2 currently!
+     *
+     * @return string
+     */
+    protected function renderLegacyDisplay_Name()
+    {
+        return c1::renderKeyValue('alias', $this->display_name);
+    }
+
     protected function renderLegacyTimeout()
     {
         return '';
