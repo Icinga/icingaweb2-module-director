3b17c880341a90d31905d2478af5c1091e24f3a8
Markus Frosch <markus.frosch@icinga.com>
2016-10-14 11:52:30 +0200

    IcingaConfigRenderer: Add toLegacyConfigString to support legacy config
    refs #12919

--- library/Director/IcingaConfig/IcingaConfigRenderer.php
+++ library/Director/IcingaConfig/IcingaConfigRenderer.php
@@ -5,5 +5,6 @@ namespace Icinga\Module\Director\IcingaConfig;
 interface IcingaConfigRenderer
 {
     public function toConfigString();
+    public function toLegacyConfigString();
     public function __toString();
 }
