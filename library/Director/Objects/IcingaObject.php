a8179507dc903a04442a635de5612ccf88c5bac1
Markus Frosch <markus.frosch@icinga.com>
2016-10-14 11:07:37 +0200

    IcingaObject(Legacy): Make getLegacyObjectType public
    refs #12919

--- library/Director/Objects/IcingaObject.php
+++ library/Director/Objects/IcingaObject.php
@@ -1955,7 +1955,7 @@ abstract class IcingaObject extends DbObject implements IcingaConfigRenderer
         );
     }
 
-    protected function getLegacyObjectType()
+    public function getLegacyObjectType()
     {
         return strtolower($this->getType());
     }
