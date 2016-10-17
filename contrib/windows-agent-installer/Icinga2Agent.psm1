160a630c5cc67c5a1ff2c7aed3c074915c6ae4da
Lord Hepipud <lord-hepipud@phiadariasoft.com>
2016-10-17 15:05:01 +0200

    Icinga2Agent.psm1: <nscp> and <windows-plugins>
    fixes #12476

--- contrib/windows-agent-installer/Icinga2Agent.psm1
+++ contrib/windows-agent-installer/Icinga2Agent.psm1
@@ -531,6 +531,8 @@ function Icinga2AgentModule {
 include "constants.conf"
 include <itl>
 include <plugins>
+include <nscp>
+include <windows-plugins>
 // include <plugins-contrib>
 if (!globals.contains("NscpPath")) {
   NscpPath = dirname(msi_get_component_path("{5C45463A-4AE9-4325-96DB-6E239C034F93}"))
