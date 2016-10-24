6e83a4fd5b6b56dd84c5e364285abf153d89591f
Thomas Gelf <thomas@gelf.net>
2016-10-24 21:55:02 +0000

    IcingaObjectGroup: provide groups for assign rules

--- library/Director/Objects/IcingaObjectGroup.php
+++ library/Director/Objects/IcingaObjectGroup.php
@@ -2,6 +2,9 @@
 
 namespace Icinga\Module\Director\Objects;
 
+use Icinga\Application\Config;
+use Icinga\Exception\IcingaException;
+use Icinga\Module\Director\Db;
 use Icinga\Module\Director\IcingaConfig\IcingaConfig;
 
 abstract class IcingaObjectGroup extends IcingaObject
@@ -20,5 +23,33 @@ abstract class IcingaObjectGroup extends IcingaObject
     public function getRenderingZone(IcingaConfig $config = null)
     {
         return $this->connection->getDefaultGlobalZoneName();
+    }
+
+    public static function enumForType($type, Db $connection = null)
+    {
+        if ($connection === null) {
+            // TODO: not nice :(
+            $connection = Db::fromResourceName(
+                Config::module('director')->get('db', 'resource')
+            );
+        }
+
+        // Last resort defense against potentiall lousy checks:
+        if (! ctype_alpha($type)) {
+            throw new IcingaException(
+                'Holy shit, you should never have reached this'
+            );
+        }
+
+        $db = $connection->getDbAdapter();
+        $select = $db->select()->from(
+            'icinga_' . $type . 'group',
+            array(
+                'name'    => 'object_name',
+                'display' => 'COALESCE(display_name, object_name)'
+            )
+        )->where('object_type = ?', 'object')->order('display');
+
+        return $db->fetchPairs($select);
     }
 }
