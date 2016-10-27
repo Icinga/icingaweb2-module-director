5921f4773f1a44fd5d2a9212ad81c53659ec597b
Thomas Gelf <thomas@gelf.net>
2016-10-27 19:34:11 +0000

    DbObject: one more protection when looking up...
    ...prefetched objects by name

--- library/Director/Data/Db/DbObject.php
+++ library/Director/Data/Db/DbObject.php
@@ -943,7 +943,10 @@ abstract class DbObject
     {
         $class = get_called_class();
         if (static::hasPrefetched($key)) {
-            if (is_string($key) && array_key_exists($key, self::$prefetchedNames[$class])) {
+            if (is_string($key)
+                && array_key_exists($class, self::$prefetchedNames)
+                && array_key_exists($key, self::$prefetchedNames[$class])
+            ) {
                 return self::$prefetched[$class][
                     self::$prefetchedNames[$class][$key]
                 ];
@@ -973,7 +976,10 @@ abstract class DbObject
         }
 
         if (array_key_exists($class, self::$prefetched)) {
-            if (is_string($key) && array_key_exists($key, self::$prefetchedNames[$class])) {
+            if (is_string($key)
+                && array_key_exists($class, self::$prefetchedNames)
+                && array_key_exists($key, self::$prefetchedNames[$class])
+            ) {
                 self::$prefetchStats[$class]->hitNames++;
                 return true;
             } elseif (array_key_exists($key, self::$prefetched[$class])) {
