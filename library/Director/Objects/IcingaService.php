e116e36051bc6ec4af346987b02e489734dc4cd6
Markus Frosch <markus.frosch@icinga.com>
2016-11-07 14:12:30 +0100

    IcingaService(legacy): Test for not null
    refs #13049

--- library/Director/Objects/IcingaService.php
+++ library/Director/Objects/IcingaService.php
@@ -218,7 +218,7 @@ class IcingaService extends IcingaObject
             return '';
         }
 
-        if ($this->get('assign_filter')) {
+        if ($this->get('assign_filter') !== null) {
             return $this->renderLegacyResolvedAssignFilter();
         }
 
