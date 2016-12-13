f1a61e25576574992b6fc8113a27ac52e22d24b0
Thomas Gelf <thomas@gelf.net>
2016-12-13 09:51:52 +0100

    ServiceGroup: allow to assign them to services...
    ...via assignment rules
    
    fixes #13415

--- library/Director/Objects/IcingaServiceGroup.php
+++ library/Director/Objects/IcingaServiceGroup.php
@@ -5,4 +5,9 @@ namespace Icinga\Module\Director\Objects;
 class IcingaServiceGroup extends IcingaObjectGroup
 {
     protected $table = 'icinga_servicegroup';
+
+    public function supportsAssignments()
+    {
+        return true;
+    }
 }
