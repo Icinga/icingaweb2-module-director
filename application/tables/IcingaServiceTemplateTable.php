36e79b97cdf286f1e0f25b893946913666742c77
Thomas Gelf <thomas@gelf.net>
2016-12-14 21:19:12 +0100

    IcingaServiceTemplateTable: do not override...
    ...anything at all. Now you're sooo empty. Soon you'll die... har har har...
    
    fixes #13585

--- application/tables/IcingaServiceTemplateTable.php
+++ application/tables/IcingaServiceTemplateTable.php
@@ -2,12 +2,6 @@
 
 namespace Icinga\Module\Director\Tables;
 
-use Icinga\Module\Director\Tables\IcingaServiceTable;
-
 class IcingaServiceTemplateTable extends IcingaServiceTable
 {
-    public function getBaseQuery()
-    {
-        return $this->getUnfilteredQuery()->where('s.object_type = ?', 'template');
-    }
 }
