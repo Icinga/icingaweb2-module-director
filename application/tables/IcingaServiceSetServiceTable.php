8dabfef37874a57cd54840271d3776e6537a3c55
Thomas Gelf <thomas@gelf.net>
2017-01-13 16:24:21 +0100

    IcingaServiceSetServiceTable: add host support

--- application/tables/IcingaServiceSetServiceTable.php
+++ application/tables/IcingaServiceSetServiceTable.php
@@ -2,12 +2,18 @@
 
 namespace Icinga\Module\Director\Tables;
 
+use Icinga\Module\Director\Objects\IcingaHost;
 use Icinga\Module\Director\Objects\IcingaServiceSet;
 use Icinga\Module\Director\Web\Table\QuickTable;
 
 class IcingaServiceSetServiceTable extends QuickTable
 {
     protected $set;
+
+    protected $title;
+
+    /** @var IcingaHost */
+    protected $host;
 
     protected $searchColumns = array(
         'service',
@@ -18,33 +24,58 @@ class IcingaServiceSetServiceTable extends QuickTable
         return array(
             'id'             => 's.id',
             'service_set_id' => 's.service_set_id',
+            'host_id'        => 'ss.host_id',
             'service_set'    => 'ss.object_name',
             'service'        => 's.object_name',
             'object_type'    => 's.object_type',
         );
     }
 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function setHost(IcingaHost $host)
+    {
+        $this->host = $host;
+        return $this;
+    }
+
     public function setServiceSet(IcingaServiceSet $set)
     {
         $this->set = $set;
+        $this->setId = $set->get('id');
         return $this;
     }
 
     protected function getActionUrl($row)
     {
-        $params = array(
-            'name' => $row->service,
-            'set'  => $row->service_set
-        );
-    
-        return $this->url('director/service', $params);
+        if ($this->host) {
+            $params = array(
+                'name'    => $this->host->getObjectName(),
+                'service' => $row->service,
+                'set'     => $row->service_set
+            );
+
+            return $this->url('director/host/servicesetservice', $params);
+
+        } else {
+            $params = array(
+                'name' => $row->service,
+                'set'  => $row->service_set
+            );
+
+            return $this->url('director/service', $params);
+        }
     }
 
     public function getTitles()
     {
         $view = $this->view();
         return array(
-            'service' => $view->translate('Servicename'),
+            'service' => $this->title ?: $view->translate('Servicename'),
         );
     }
 
