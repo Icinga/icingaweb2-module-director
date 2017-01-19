334bd9f58db8842cbb2186bae4bac7235bfb3082
Thomas Gelf <thomas@gelf.net>
2017-01-19 12:13:17 +0100

    schema/upgrade_104: workaround for PostgreSQL
    refs #730

--- schema/pgsql-migrations/upgrade_104.sql
+++ schema/pgsql-migrations/upgrade_104.sql
@@ -1,10 +1,16 @@
 ALTER TABLE icinga_timeperiod_range
-  ADD COLUMN range_key character varying(255) NOT NULL,
-  ADD COLUMN range_value character varying(255) NOT NULL;
+  ADD COLUMN range_key character varying(255) DEFAULT NULL,
+  ADD COLUMN range_value character varying(255) DEFAULT NULL;
 
 UPDATE icinga_timeperiod_range
   SET range_key = timeperiod_key,
     range_value = timeperiod_value;
+
+ALTER TABLE icinga_timeperiod_range
+  ALTER COLUMN range_key SET NOT NULL,
+  ALTER COLUMN range_key DROP DEFAULT,
+  ALTER COLUMN range_value SET NOT NULL,
+  ALTER COLUMN range_value DROP DEFAULT;
 
 ALTER TABLE icinga_timeperiod_range
   DROP CONSTRAINT icinga_timeperiod_range_pkey,
