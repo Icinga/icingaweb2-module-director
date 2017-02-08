f2bbd2d5d7af13017b2e9353df6b6877aac6aa2d
Thomas Gelf <thomas@gelf.net>
2017-02-08 16:42:00 +0100

    test: add compatibility layer for phpunit

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,6 +4,9 @@ use Icinga\Module\Director\Test\Bootstrap;
 
 call_user_func(function () {
     $basedir = dirname(__DIR__);
+    if (! class_exists('PHPUnit_Framework_TestCase')) {
+        require_once __DIR__ . '/phpunit-compat.php';
+    }
     require_once $basedir . '/library/Director/Test/Bootstrap.php';
     Bootstrap::cli($basedir);
 });
---
+++ test/phpunit-compat.php
@@ -0,0 +1,10 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @codingStandardsIgnoreStart
+ */
+class PHPUnit_Framework_TestCase extends TestCase
+{
+}
