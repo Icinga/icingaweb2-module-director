25395656ce1879b207c9d1ea07157210e51bd53f
Thomas Gelf <thomas@gelf.net>
2017-02-15 12:50:27 +0100

    CustomVariable: fix rendering for keys starting...
    ...with numbers
    
    fixes #792

--- library/Director/CustomVariable/CustomVariable.php
+++ library/Director/CustomVariable/CustomVariable.php
@@ -115,7 +115,7 @@ abstract class CustomVariable implements IcingaConfigRenderer
 
     protected function renderKeyName($key)
     {
-        if (preg_match('/^[a-z0-9_]+\d*$/i', $key)) {
+        if (preg_match('/^[a-z][a-z0-9_]*$/i', $key)) {
             return 'vars.' . c::escapeIfReserved($key);
         } else {
             return 'vars[' . c::renderString($key) . ']';
