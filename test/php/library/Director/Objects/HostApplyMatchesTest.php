a99252ee5e0c6545d8af8cc0ddb37c221d233e61
Thomas Gelf <thomas@gelf.net>
2017-03-03 09:07:43 +0100

    HostApplyMatchesTest: new tests

---
+++ test/php/library/Director/Objects/HostApplyMatchesTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Icinga\Module\Director\Objects;
+
+use Icinga\Data\Filter\Filter;
+use Icinga\Module\Director\Objects\HostApplyMatches;
+use Icinga\Module\Director\Objects\IcingaHost;
+use Icinga\Module\Director\Test\BaseTestCase;
+
+class HostApplyMatchesTest extends BaseTestCase
+{
+    public function testExactMatches()
+    {
+        $matcher = HostApplyMatches::prepare($this->sampleHost());
+        $this->assertTrue(
+            $matcher->matchesFilter(
+                Filter::fromQueryString('host.name=%22aha%22')
+            )
+        );
+        $this->assertFalse(
+            $matcher->matchesFilter(
+                Filter::fromQueryString('host.name=%22ahaa%22')
+            )
+        );
+    }
+
+    public function testWildcardMatches()
+    {
+        $matcher = HostApplyMatches::prepare($this->sampleHost());
+        $this->assertTrue(
+            $matcher->matchesFilter(
+                Filter::fromQueryString('host.name=%22ah*%22')
+            )
+        );
+        $this->assertTrue(
+            $matcher->matchesFilter(
+                Filter::fromQueryString('host.name=%22*h*%22')
+            )
+        );
+        $this->assertFalse(
+            $matcher->matchesFilter(
+                Filter::fromQueryString('host.name=%22*g*%22')
+            )
+        );
+    }
+
+    public function testStringVariableMatches()
+    {
+        $matcher = HostApplyMatches::prepare($this->sampleHost());
+        $this->assertTrue(
+            $matcher->matchesFilter(
+                Filter::fromQueryString('host.vars.location=%22*urem*%22')
+            )
+        );
+        $this->assertTrue(
+            $matcher->matchesFilter(
+                Filter::fromQueryString('host.vars.location=%22Nuremberg%22')
+            )
+        );
+        $this->assertFalse(
+            $matcher->matchesFilter(
+                Filter::fromQueryString('host.vars.location=%22Nurembergg%22')
+            )
+        );
+    }
+
+    protected function sampleHost()
+    {
+        return IcingaHost::create(array(
+            'object_type' => 'object',
+            'object_name' => 'aha',
+            'vars' => array(
+                'location' => 'Nuremberg',
+                'tags' => array('Special', 'Amazing'),
+            )
+        ));
+    }
+}
