Icinga/icingaweb2-module-director

commit e6e51dc2be7fc2e859ce47dba9c25c3b9e35cfcb

SyncPropertyForm: fix error on list sync
fixes #836

application/forms/SyncPropertyForm.php
@@ -288,24 +288,29 @@ class SyncPropertyForm extends DirectorObjectForm
         }
 
         foreach ($dummy->listProperties() as $prop) {
-            if ($prop === 'id') {
+            if ($dummy instanceof IcingaObject && $prop === 'id') {
                 continue;
             }
 
             // TODO: allow those fields, but munge them (store ids)
             //if (preg_match('~_id$~', $prop)) continue;
             if (substr($prop, -3) === '_id') {
-                $prop = substr($prop, 0, -3);
-                if (! $dummy instanceof IcingaObject || ! $dummy->hasRelation($prop)) {
-                    continue;
+                if ($dummy instanceof IcingaObject) {
+                    if ($dummy->hasRelation($prop)) {
+                        $prop = substr($prop, 0, -3);
+                    } else {
+                        continue;
+                    }
                 }
             }
 
             $props[$prop] = $prop;
         }
 
-        foreach ($dummy->listMultiRelations() as $prop) {
-            $props[$prop] = sprintf('%s (%s)', $prop, $this->translate('a list'));
+        if ($dummy instanceof IcingaObject) {
+            foreach ($dummy->listMultiRelations() as $prop) {
+                $props[$prop] = sprintf('%s (%s)', $prop, $this->translate('a list'));
+            }
         }
 
         ksort($props);

commit ddcaaa7bcf9d14a19233718937c04877aa08873e

SyncPropertyForm: re-add related fields
e6e51dc broke this, related fields like 'host' or 'zone' have no longer been
offered. This did not affect existing Sync Rule configurations

fixes #903

application/forms/SyncPropertyForm.php
@@ -295,9 +295,10 @@ class SyncPropertyForm extends DirectorObjectForm
             // TODO: allow those fields, but munge them (store ids)
             //if (preg_match('~_id$~', $prop)) continue;
             if (substr($prop, -3) === '_id') {
+                $short = substr($prop, 0, -3);
                 if ($dummy instanceof IcingaObject) {
-                    if ($dummy->hasRelation($prop)) {
-                        $prop = substr($prop, 0, -3);
+                    if ($dummy->hasRelation($short)) {
+                        $prop = $short;
                     } else {
                         continue;
                     }