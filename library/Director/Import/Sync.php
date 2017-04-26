Icinga/icingaweb2-module-director

commit f885d90a0a3534da8aee74889a2441f313a3b9c7

Sync: also fetch columns used in filters only
Filters might otherwise in some circumstances not work as expected

fixes #876

library/Director/Import/Sync.php
@@ -76,6 +76,7 @@ class Sync
 
     protected $runStartTime;
 
+    /** @var Filter[] */
     protected $columnFilters = array();
 
     /**
@@ -258,9 +259,20 @@ class Sync
             $key = $source->key_column;
             $this->sourceColumns[$sourceId][$key] = $key;
             $run = $source->fetchLastRun(true);
-            $rows = $run->fetchRows(
-                SyncUtils::getRootVariables($this->sourceColumns[$sourceId])
-            );
+
+            $usedColumns = SyncUtils::getRootVariables($this->sourceColumns[$sourceId]);
+
+            $filterColumns = array();
+            foreach ($this->columnFilters as $filter) {
+                foreach ($filter->listFilteredColumns() as $column) {
+                    $filterColumns[$column] = $column;
+                }
+            }
+            foreach (SyncUtils::getRootVariables($filterColumns) as $column) {
+                $usedColumns[$column] = $column;
+            }
+
+            $rows = $run->fetchRows($usedColumns);
 
             $this->imported[$sourceId] = array();
             foreach ($rows as $row) {

commit 6de258730be7ab24b766b3d5ce526743068c631a

Sync: fix issues with PHP 5.3
Problem has been introduced with f885d90

fixes #906

library/Director/Import/Sync.php
@@ -268,8 +268,10 @@ class Sync
                     $filterColumns[$column] = $column;
                 }
             }
-            foreach (SyncUtils::getRootVariables($filterColumns) as $column) {
-                $usedColumns[$column] = $column;
+            if (! empty($filterColumns)) {
+                foreach (SyncUtils::getRootVariables($filterColumns) as $column) {
+                    $usedColumns[$column] = $column;
+                }
             }
 
             $rows = $run->fetchRows($usedColumns);

library/Director/Import/SyncUtils.php
@@ -125,6 +125,10 @@ class SyncUtils
             }
         }
 
+        if (empty($res)) {
+            return array();
+        }
+
         return array_combine($res, $res);
     }
 }