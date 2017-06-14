7502706284a2e162089af1ba7e17cc520a703d91
Thomas Gelf <thomas@gelf.net>
2017-06-14 13:40:43 +0200

    QuickSearch: add a trait for tables too

---
+++ library/Director/Web/Table/Extension/QuickSearch.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Icinga\Module\Director\Web\Table\Extension;
+
+use ipl\Html\BaseElement;
+use ipl\Html\Html;
+use ipl\Web\Url;
+
+trait QuickSearch
+{
+    /** @var BaseElement */
+    private $quickSearchForm;
+
+    public function getQuickSearch(BaseElement $parent, Url $url)
+    {
+        $this->requireQuickSearchForm($parent, $url);
+        $search = $url->getParam('q');
+        return $search;
+    }
+
+    private function requireQuickSearchForm(BaseElement $parent, Url $url)
+    {
+        if ($this->quickSearchForm === null) {
+            $this->quickSearchForm = $this->buildQuickSearchForm($parent, $url);
+        }
+    }
+
+    private function buildQuickSearchForm(BaseElement $parent, Url $url)
+    {
+        $search = $url->getParam('q');
+
+        $form = Html::tag('form', [
+            'action' => $url->without(array('q', 'page', 'modifyFilter')),
+            'class'  => ['quicksearch', 'inline'],
+            'method' => 'GET'
+        ]);
+
+        $form->add(
+            Html::tag('input', [
+                'type' => 'text',
+                'name' => 'q',
+                'value' => $search,
+                'placeholder' => $this->translate('Search...'),
+                'class' => 'search'
+            ])
+        );
+
+        $parent->prepend($form);
+
+        return $form;
+    }
+}
