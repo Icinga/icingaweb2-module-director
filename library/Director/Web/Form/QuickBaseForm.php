4590fa1d739be955406016ff08eedb8963a7a7fb
Thomas Gelf <thomas@gelf.net>
2017-06-14 18:27:54 +0200

    QuickForm: guess what? We are ValidHtml!

--- library/Director/Web/Form/QuickBaseForm.php
+++ library/Director/Web/Form/QuickBaseForm.php
@@ -4,9 +4,10 @@ namespace Icinga\Module\Director\Web\Form;
 
 use Icinga\Application\Icinga;
 use Icinga\Application\Modules\Module;
+use ipl\Html\ValidHtml;
 use Zend_Form;
 
-abstract class QuickBaseForm extends Zend_Form
+abstract class QuickBaseForm extends Zend_Form implements ValidHtml
 {
     /**
      * The Icinga module this form belongs to. Usually only set if the
