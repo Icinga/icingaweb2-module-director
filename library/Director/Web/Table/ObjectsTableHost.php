11b054d296f67ead8d1694a85d2a7228d9b881d4
Thomas Gelf <thomas@gelf.net>
2017-06-19 00:54:05 +0200

    ObjectsTableHost: new table

---
+++ library/Director/Web/Table/ObjectsTableHost.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Icinga\Module\Director\Web\Table;
+
+use Icinga\Module\Director\Web\Table\Extension\MultiSelect;
+
+class ObjectsTableHost extends ObjectsTable
+{
+    use MultiSelect;
+
+    protected $searchColumns = [
+        'o.object_name',
+        'o.display_name',
+        'o.address',
+    ];
+
+    protected $columns = [
+        'object_name'  => 'o.object_name',
+        'display_name' => 'o.display_name',
+        'address'      => 'o.address',
+        'id'           => 'o.id',
+    ];
+
+    protected $showColumns = [
+        'object_name' => 'Hostname',
+        'address'     => 'Address'
+    ];
+
+    protected function assemble()
+    {
+        $this->enableMultiSelect(
+            'director/hosts/edit',
+            'director/hosts',
+            ['name']
+        );
+    }
+
+    // Restrictions!
+    // foreach ($this->objectRestrictions as $restriction) {
+    //     $restriction->applyToHostsQuery($query);
+    // }
+}
