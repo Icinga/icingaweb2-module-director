61b1e4b581a9cd9dbed48452a2cb305a1371bd4e
Thomas Gelf <thomas@gelf.net>
2017-06-21 22:57:32 +0200

    ServicesController: remove obsolete code

--- application/controllers/ServicesController.php
+++ application/controllers/ServicesController.php
@@ -9,20 +9,4 @@ class ServicesController extends ObjectsController
     protected $multiEdit = array(
         'disabled'
     );
-
-    public function init()
-    {
-        parent::init();
-        $this->view->tabs->remove('objects');
-    }
-
-    public function indexAction()
-    {
-        $r = $this->getRequest();
-        if ($r->getActionName() !== 'templates' && ! $this->getRequest()->isApiRequest()) {
-            $this->redirectNow('director/services/templates');
-        }
-
-        return parent::indexAction();
-    }
 }
