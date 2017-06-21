92982c37aaf3445494ccc6e8a33dcc089b51e9b6
Thomas Gelf <thomas@gelf.net>
2017-06-21 22:03:47 +0200

    ObjectRestriction: work with Zf Select

--- library/Director/Restriction/ObjectRestriction.php
+++ library/Director/Restriction/ObjectRestriction.php
@@ -5,8 +5,10 @@ namespace Icinga\Module\Director\Restriction;
 use Icinga\Authentication\Auth;
 use Icinga\Exception\ProgrammingError;
 use Icinga\Module\Director\Db;
+use Icinga\Module\Director\Objects\IcingaObject;
+use Zend_Db_Select as ZfSelect;
 
-class ObjectRestriction
+abstract class ObjectRestriction
 {
     /** @var string */
     protected $name;
@@ -23,6 +25,29 @@ class ObjectRestriction
         $this->auth = $auth;
     }
 
+    abstract public function allows(IcingaObject $object);
+
+    /**
+     * Apply the restriction to the given Hosts Query
+     *
+     * We assume that the query wants to fetch hosts and that therefore the
+     * icinga_host table already exists in the given query, using the $tableAlias
+     * alias.
+     *
+     * @param ZfSelect $query
+     * @param string $tableAlias
+     */
+    abstract protected function filterQuery(ZfSelect $query, $tableAlias = 'o');
+
+    public function applyToQuery(ZfSelect $query, $tableAlias = 'o')
+    {
+        if ($this->isRestricted()) {
+            $this->filterQuery($query, $tableAlias);
+        }
+
+        return $query;
+    }
+
     public function getName()
     {
         if ($this->name === null) {
@@ -36,6 +61,20 @@ class ObjectRestriction
     {
         $restrictions = $this->auth->getRestrictions($this->getName());
         return ! empty($restrictions);
+    }
+
+    protected function getQueryTableByAlias(ZfSelect $query, $tableAlias)
+    {
+        $from = $query->getPart(ZfSelect::FROM);
+        if (! array_key_exists($tableAlias, $from)) {
+            throw new ProgrammingError(
+                'Cannot restrict query with alias "%s", got %s',
+                $tableAlias,
+                json_encode($from)
+            );
+        }
+
+        return $from[$tableAlias]['tableName'];
     }
 
     protected function gracefullySplitOnComma($string)
