5938f5051a919e27e477b9e4ec7e0ca347e8ca70
Thomas Gelf <thomas@gelf.net>
2017-07-14 08:59:06 +0200

    ActionBar: add setBaseTarget() helper

--- library/vendor/ipl/Web/Component/ActionBar.php
+++ library/vendor/ipl/Web/Component/ActionBar.php
@@ -12,4 +12,14 @@ class ActionBar extends BaseElement
     protected $tag = 'div';
 
     protected $defaultAttributes = array('class' => 'action-bar');
+
+    /**
+     * @param  string $target
+     * @return $this
+     */
+    public function setBaseTarget($target)
+    {
+        $this->attributes()->set('data-base-target', $target);
+        return $this;
+    }
 }
