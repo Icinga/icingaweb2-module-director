27d1297303d69acca760c94e4b6eaec22c4d811c
Thomas Gelf <thomas@gelf.net>
2017-07-20 11:06:58 +0200

    InfraTabs: add missing tab class

---
+++ library/Director/Web/Tabs/InfraTabs.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Icinga\Module\Director\Web\Tabs;
+
+use Icinga\Authentication\Auth;
+use ipl\Translation\TranslationHelper;
+use ipl\Web\Widget\Tabs;
+
+class InfraTabs extends Tabs
+{
+    use TranslationHelper;
+
+    /** @var Auth */
+    protected $auth;
+
+    public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+        // We are not a BaseElement, not yet
+        $this->assemble();
+    }
+
+    protected function assemble()
+    {
+        $auth = $this->auth;
+
+        if ($auth->hasPermission('director/audit')) {
+            $this->add('activitylog', [
+                'label' => $this->translate('Activity Log'),
+                'url'   => 'director/config/activities'
+            ]);
+        }
+
+        if ($auth->hasPermission('director/deploy')) {
+            $this->add('deploymentlog', [
+                'label' => $this->translate('Deployments'),
+                'url' => 'director/config/deployments'
+            ]);
+        }
+
+        if ($auth->hasPermission('director/admin')) {
+            $this->add('infrastructure', [
+                'label'     => $this->translate('Infrastructure'),
+                'url'       => 'director/dashboard',
+                'urlParams' => ['name' => 'infrastructure']
+            ]);
+        }
+    }
+}
