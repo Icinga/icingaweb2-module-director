806bca25554fcfac317d93a61869825e57a830b7
Thomas Gelf <thomas@gelf.net>
2017-08-04 11:13:28 +0200

    StartupLogRenderer: alias for Util class
    Should help to work around PHP bugs in some older versions
    
    fixes #1057

--- library/Director/StartupLogRenderer.php
+++ library/Director/StartupLogRenderer.php
@@ -4,7 +4,7 @@ namespace Icinga\Module\Director;
 
 use Icinga\Module\Director\Objects\DirectorDeploymentLog;
 use ipl\Html\Link;
-use ipl\Html\Util;
+use ipl\Html\Util as iplUtil;
 use ipl\Html\ValidHtml;
 
 class StartupLogRenderer implements ValidHtml
@@ -20,7 +20,7 @@ class StartupLogRenderer implements ValidHtml
     public function render()
     {
         $deployment = $this->deployment;
-        $log = Util::escapeForHtml($deployment->get('startup_log'));
+        $log = iplUtil::escapeForHtml($deployment->get('startup_log'));
         $lines = array();
         $severity = 'information';
         $sevPattern = '/^(debug|notice|information|warning|critical)\/(\w+)/';
