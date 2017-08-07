2acc6a2c07820846891756b951ec247469a20dd4
Thomas Gelf <thomas@gelf.net>
2017-08-07 09:55:01 +0200

    ServiceObjectDashlet: show even if permissions...
    ...are only granted for sets and not for services

--- library/Director/Dashboard/Dashlet/ServiceObjectDashlet.php
+++ library/Director/Dashboard/Dashlet/ServiceObjectDashlet.php
@@ -2,6 +2,8 @@
 
 namespace Icinga\Module\Director\Dashboard\Dashlet;
 
+use Icinga\Module\Director\Acl;
+
 class ServiceObjectDashlet extends Dashlet
 {
     protected $icon = 'services';
@@ -21,5 +23,12 @@ class ServiceObjectDashlet extends Dashlet
     public function listRequiredPermissions()
     {
         return ['director/services'];
+    }
+
+    public function isAllowed()
+    {
+        $acl = Acl::instance();
+        return $acl->hasPermission('director/services')
+            || $acl->hasPermission('director/service_sets');
     }
 }
