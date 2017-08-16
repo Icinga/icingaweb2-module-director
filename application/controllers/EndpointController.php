cea6baa331a47cc621e1f34964835ba7a44d7341
Thomas Gelf <thomas@gelf.net>
2017-08-16 14:43:03 +0200

    EndpointController: remove obsolete init()

--- application/controllers/EndpointController.php
+++ application/controllers/EndpointController.php
@@ -6,12 +6,4 @@ use Icinga\Module\Director\Web\Controller\ObjectController;
 
 class EndpointController extends ObjectController
 {
-    public function init()
-    {
-        $this->assertPermission('director/inspect');
-        parent::init();
-        if ($this->object && $this->object->hasApiUser()) {
-            $params['endpoint'] = $this->object->object_name;
-        }
-    }
 }
