4c177fc4ea8713e72beefe27eca4c5aa9e39dbe8
Thomas Gelf <thomas@gelf.net>
2017-08-16 14:14:30 +0200

    JobsController: refactor, cleanup

--- application/controllers/JobsController.php
+++ application/controllers/JobsController.php
@@ -3,27 +3,18 @@
 namespace Icinga\Module\Director\Controllers;
 
 use Icinga\Module\Director\Web\Controller\ActionController;
+use Icinga\Module\Director\Web\Table\JobTable;
+use Icinga\Module\Director\Web\Tabs\ImportTabs;
 
 class JobsController extends ActionController
 {
     public function indexAction()
     {
-        $this->setAutoRefreshInterval(10);
-        $this->view->title = $this->translate('Jobs');
-        $this->setImportTabs()->activate('jobs');
+        $this->addTitle($this->translate('Jobs'))
+            ->setAutoRefreshInterval(10)
+            ->addAddLink($this->translate('Add a new Job'), 'director/job/add')
+            ->tabs(new ImportTabs())->activate('jobs');
 
-        $this->view->addLink = $this->view->qlink(
-            $this->translate('Add'),
-            'director/job',
-            null,
-            array('class' => 'icon-plus')
-        );
-
-        $this->view->table = $this->applyPaginationLimits(
-            $this->loadTable('job')
-                ->setConnection($this->db())
-        );
-
-        $this->setViewScript('list/table');
+        (new JobTable($this->db()))->renderTo($this);
     }
 }
