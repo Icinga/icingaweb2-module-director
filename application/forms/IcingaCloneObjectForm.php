e6279d87e3cffa43991abbfde7b08784e93f7894
Thomas Gelf <thomas@gelf.net>
2017-08-18 16:11:57 +0200

    IcingaCloneObjectForm: allow to clone single...
    ...sets and services with their host. Also, only allow admins to clone fully
    resolved
    
    fixes #629

--- application/forms/IcingaCloneObjectForm.php
+++ application/forms/IcingaCloneObjectForm.php
@@ -2,8 +2,11 @@
 
 namespace Icinga\Module\Director\Forms;
 
+use Icinga\Module\Director\Acl;
 use Icinga\Module\Director\Objects\IcingaHost;
 use Icinga\Module\Director\Objects\IcingaObject;
+use Icinga\Module\Director\Objects\IcingaService;
+use Icinga\Module\Director\Objects\IcingaServiceSet;
 use Icinga\Module\Director\Web\Form\DirectorForm;
 
 class IcingaCloneObjectForm extends DirectorForm
@@ -20,14 +23,31 @@ class IcingaCloneObjectForm extends DirectorForm
             'value'    => $name,
         ));
 
-        $this->addElement('select', 'clone_type', array(
-            'label'    => 'Clone type',
-            'required' => true,
-            'multiOptions' => array(
-                'equal' => $this->translate('Clone the object as is, preserving imports'),
-                'flat'  => $this->translate('Flatten all inherited properties, strip imports'),
-            )
-        ));
+        if (Acl::instance()->hasPermission('director/admin')) {
+            $this->addElement('select', 'clone_type', array(
+                'label'    => 'Clone type',
+                'required' => true,
+                'multiOptions' => array(
+                    'equal' => $this->translate('Clone the object as is, preserving imports'),
+                    'flat'  => $this->translate('Flatten all inherited properties, strip imports'),
+                )
+            ));
+        }
+
+        if ($this->object instanceof IcingaHost) {
+            $this->addBoolean('clone_services', [
+                'label'       => $this->translate('Clone Services'),
+                'description' => $this->translate(
+                    'Also clone single Services defined for this Host'
+                )
+            ], 'y');
+            $this->addBoolean('clone_service_sets', [
+                'label'       => $this->translate('Clone Service Sets'),
+                'description' => $this->translate(
+                    'Also clone single Service Sets defined for this Host'
+                )
+            ], 'y');
+        }
 
         $this->submitLabel = sprintf(
             $this->translate('Clone "%s"'),
@@ -37,10 +57,11 @@ class IcingaCloneObjectForm extends DirectorForm
 
     public function onSuccess()
     {
-
         $object = $this->object;
+        $connection = $object->getConnection();
         $newname = $this->getValue('new_object_name');
-        $resolve = $this->getValue('clone_type') === 'flat';
+        $resolve = Acl::instance()->hasPermission('director/admin')
+            && $this->getValue('clone_type') === 'flat';
 
         $msg = sprintf(
             'The %s "%s" has been cloned from "%s"',
@@ -51,14 +72,39 @@ class IcingaCloneObjectForm extends DirectorForm
 
         $new = $object::fromPlainObject(
             $object->toPlainObject($resolve),
-            $object->getConnection()
+            $connection
         )->set('object_name', $newname);
 
-        if ($new instanceof IcingaHost) {
+        if ($object instanceof IcingaHost) {
             $new->set('api_key', null);
+            if ($this->getValue('clone_services') === 'y') {
+                $services = $object->fetchServices();
+            } else {
+                $services = [];
+            }
+            if ($this->getValue('clone_service_sets') === 'y') {
+                $sets = $object->fetchServiceSets();
+            } else {
+                $sets = [];
+            }
+        } else {
+            $services = [];
+            $sets = [];
         }
 
         if ($new->store()) {
+            foreach ($services as $service) {
+                IcingaService::fromPlainObject(
+                    $service->toPlainObject(),
+                    $connection
+                )->set('host_id', $new->get('id'))->store();
+            }
+            foreach ($sets as $set) {
+                IcingaServiceSet::fromPlainObject(
+                    $set->toPlainObject(),
+                    $connection
+                )->set('host_id', $new->get('id'))->store();
+            }
             $this->setSuccessUrl(
                 'director/' . strtolower($object->getShortTableName()),
                 $new->getUrlParams()
