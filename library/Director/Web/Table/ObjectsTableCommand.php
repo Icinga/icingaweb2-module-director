d65114e7834aaeca42e51b402d6a9446862d4a97
Thomas Gelf <thomas@gelf.net>
2017-08-25 10:51:21 +0200

    TableCommand: do not search for object_type
    fixes #1109

--- library/Director/Web/Table/ObjectsTableCommand.php
+++ library/Director/Web/Table/ObjectsTableCommand.php
@@ -6,10 +6,9 @@ use Zend_Db_Select as ZfSelect;
 
 class ObjectsTableCommand extends ObjectsTable
 {
-    // TODO: external commands? Notifications separately?
+    // TODO: Notifications separately?
     protected $searchColumns = [
         'o.object_name',
-        'o.object_type',
         'o.command',
     ];
 
