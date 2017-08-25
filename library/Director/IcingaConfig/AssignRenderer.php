a6222e2abbb57b598212c49e6ba90fe461050639
Thomas Gelf <thomas@gelf.net>
2017-08-25 16:22:22 +0200

    AssignRenderer: special treatment for not in group
    fixes #686

--- library/Director/IcingaConfig/AssignRenderer.php
+++ library/Director/IcingaConfig/AssignRenderer.php
@@ -75,6 +75,23 @@ class AssignRenderer
         }
     }
 
+    protected function renderNotEquals($column, $expression)
+    {
+        if (substr($column, -7) === '.groups') {
+            return sprintf(
+                '!(%s in %s)',
+                $expression,
+                $column
+            );
+        } else {
+            return sprintf(
+                '%s != %s',
+                $column,
+                $expression
+            );
+        }
+    }
+
     protected function renderInArray($column, $expression)
     {
         return sprintf(
@@ -147,11 +164,7 @@ class AssignRenderer
             }
         } elseif ($filter instanceof FilterMatchNot) {
             if (strpos($expression, '*') === false) {
-                return sprintf(
-                    '%s != %s',
-                    $column,
-                    $expression
-                );
+                return $this->renderNotEquals($column, $expression);
             } else {
                 return sprintf(
                     '! match(%s, %s)',
