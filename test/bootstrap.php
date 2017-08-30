bd5f9d7deaf48bf83c96f819cba4af0d3911e2c0
Markus Frosch <markus.frosch@icinga.com>
2017-08-30 09:35:47 +0200

    testing: Move vendor installation to script
    And all downloaded data to `vendor/`

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -7,6 +7,10 @@ call_user_func(function () {
     if (! class_exists('PHPUnit_Framework_TestCase')) {
         require_once __DIR__ . '/phpunit-compat.php';
     }
+
+    $include_path = $basedir . '/vendor' . PATH_SEPARATOR . ini_get('include_path');
+    ini_set('include_path', $include_path);
+
     require_once $basedir . '/library/Director/Test/Bootstrap.php';
     Bootstrap::cli($basedir);
 });
