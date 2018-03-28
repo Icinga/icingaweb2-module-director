2f68489cac8a6fcb5f310f830da65bba11ac855c
Thomas Gelf <thomas@gelf.net>
2018-03-28 17:55:57 +0200

    DirectorDb: do not fail when unconfigured and...
    ...not use by Controllers

--- library/Director/Web/Controller/Extension/DirectorDb.php
+++ library/Director/Web/Controller/Extension/DirectorDb.php
@@ -4,6 +4,7 @@ namespace Icinga\Module\Director\Web\Controller\Extension;
 
 use Icinga\Exception\ConfigurationError;
 use Icinga\Module\Director\Db;
+use Icinga\Module\Director\Web\Controller\ActionController;
 
 trait DirectorDb
 {
@@ -21,12 +22,14 @@ trait DirectorDb
             $resourceName = $this->Config()->get('db', 'resource');
             if ($resourceName) {
                 $this->db = Db::fromResourceName($resourceName);
-            } else {
+            } elseif ($this instanceof ActionController) {
                 if ($this->getRequest()->isApiRequest()) {
                     throw new ConfigurationError('Icinga Director is not correctly configured');
                 } else {
                     $this->redirectNow('director');
                 }
+            } else {
+                throw new ConfigurationError('Icinga Director is not correctly configured');
             }
         }
 
