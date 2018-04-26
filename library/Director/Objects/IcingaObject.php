ff0553c9dd42da17a451cbdc8d7a2183899e943b
Markus Frosch <markus.frosch@icinga.com>
2018-04-26 16:46:50 +0200

    legacy/IcingaObject: Render interval properties properly

--- library/Director/Objects/IcingaObject.php
+++ library/Director/Objects/IcingaObject.php
@@ -2075,6 +2075,13 @@ abstract class IcingaObject extends DbObject implements IcingaConfigRenderer
             }
         }
 
+        if ($this->propertyIsInterval($key)) {
+            return c1::renderKeyValue(
+                $this->intervalProperties[$key],
+                c1::renderInterval($value)
+            );
+        }
+
         if (substr($key, -3) === '_id'
              && $this->hasRelation($relKey = substr($key, 0, -3))
         ) {
