5a3e604e754fe15e379c17d13e7a1d08e90a90ec
Thomas Gelf <thomas@gelf.net>
2018-06-13 21:17:41 +0200

    Syncrules: allow export via REST API

--- application/controllers/SyncrulesController.php
+++ application/controllers/SyncrulesController.php
@@ -2,14 +2,26 @@
 
 namespace Icinga\Module\Director\Controllers;
 
+use Icinga\Module\Director\DirectorObject\Automation\ImportExport;
 use Icinga\Module\Director\Web\Table\SyncruleTable;
 use Icinga\Module\Director\Web\Controller\ActionController;
 use Icinga\Module\Director\Web\Tabs\ImportTabs;
 
 class SyncrulesController extends ActionController
 {
+    protected $isApified = true;
+
+    /**
+     * @throws \Icinga\Exception\ConfigurationError
+     * @throws \Icinga\Exception\Http\HttpNotFoundException
+     */
     public function indexAction()
     {
+        if ($this->getRequest()->isApiRequest()) {
+            $this->sendExport();
+            return;
+        }
+
         $this->addTitle($this->translate('Sync rule'))
             ->setAutoRefreshInterval(10)
             ->addAddLink(
@@ -18,5 +30,16 @@ class SyncrulesController extends ActionController
             )->tabs(new ImportTabs())->activate('syncrule');
 
         (new SyncruleTable($this->db()))->renderTo($this);
+    }
+
+    /**
+     * @throws \Icinga\Exception\ConfigurationError
+     */
+    protected function sendExport()
+    {
+        $this->sendJson(
+            $this->getResponse(),
+            (new ImportExport($this->db()))->serializeAllSyncRules()
+        );
     }
 }
