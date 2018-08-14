Icinga/icingaweb2-module-director

commit fd372c6b6286f9d8e7fbe1c9dead719bc5127790

IcingaConfig: check whether get_host succeeded...
...and warn otherwise

fixes #1595

library/Director/IcingaConfig/IcingaConfig.php
@@ -533,18 +533,35 @@ globals.directorWarnOnceForThresholds = function() {
 
         return sprintf(
             '
-if (! globals["DirectorOverrideTemplate"]) {
-  const DirectorOverrideVars = "%s"
   const DirectorOverrideTemplate = "%s"
+if (! globals[DirectorOverrideTemplate]) {
+  const DirectorOverrideVars = "%s"
+
+  globals.directorWarnedOnceForServiceWithoutHost = false;
+  globals.directorWarnOnceForServiceWithoutHost = function() {
+    if (globals.directorWarnedOnceForServiceWithoutHost == false) {
+      globals.directorWarnedOnceForServiceWithoutHost = true
+      log(
+        LogWarning,
+        "config",
+        "Director: Custom Variable Overrides will not work in this Icinga 2 version. See Director issue #1579"
+      )
+    }
+  }
 
   template Service DirectorOverrideTemplate ignore_on_error {
     /**
      * Seems that host is missing when used in a service object, works fine for
      * apply rules
      */
-    if (! host) {
-      var host = get_host(host_name)
-    }
+    try {
+      if (! host) {
+        var host = get_host(host_name)
+      }
+      if (! host) {
+        globals.directorWarnOnceForServiceWithoutHost()
+      }
+    } except { globals.directorWarnOnceForServiceWithoutHost() }
 
     if (vars) {
       vars += host.vars[DirectorOverrideVars][name]

commit bf68dbcaca0a8dc85c8427042a3012cd482ff225

IcingaConfig: fix Service Override template name
Got broken by fd372c6 for #1579

fixes #1601

library/Director/IcingaConfig/IcingaConfig.php
@@ -533,7 +533,7 @@ globals.directorWarnOnceForThresholds = function() {
 
         return sprintf(
             '
-  const DirectorOverrideTemplate = "%s"
+const DirectorOverrideTemplate = "%s"
 if (! globals[DirectorOverrideTemplate]) {
   const DirectorOverrideVars = "%s"
 
@@ -549,7 +549,7 @@ if (! globals[DirectorOverrideTemplate]) {
     }
   }
 
-  template Service DirectorOverrideTemplate ignore_on_error {
+  template Service DirectorOverrideTemplate {
     /**
      * Seems that host is missing when used in a service object, works fine for
      * apply rules
@@ -571,8 +571,8 @@ if (! globals[DirectorOverrideTemplate]) {
   }
 }
 ',
-            $settings->override_services_varname,
-            $settings->override_services_templatename
+            $settings->override_services_templatename,
+            $settings->override_services_varname
         );
     }
 