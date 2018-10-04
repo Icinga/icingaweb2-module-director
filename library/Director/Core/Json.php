4e327ed54d90fd52f4b82d82979cc79b2f7899f2
Thomas Gelf <thomas@gelf.net>
2018-10-04 06:47:50 +0200

    Json: allow null/false

--- library/Director/Core/Json.php
+++ library/Director/Core/Json.php
@@ -6,11 +6,22 @@ use Icinga\Module\Director\Exception\JsonEncodeException;
 
 class Json
 {
-    public static function encode($string)
+    public static function encode($mixed, $flags = null)
     {
-        $result = json_encode($string);
+        $result = json_encode($mixed, $flags);
 
-        if ($result === false) {
+        if ($result === false && json_last_error() !== JSON_ERROR_NONE) {
+            throw JsonEncodeException::forLastJsonError();
+        }
+
+        return $result;
+    }
+
+    public static function decode($string)
+    {
+        $result = json_decode($string);
+
+        if ($result === null && json_last_error() !== JSON_ERROR_NONE) {
             throw JsonEncodeException::forLastJsonError();
         }
 
