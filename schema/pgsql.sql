5d309b3dc7e488606af3c784e89ce99f0621fac4
Thomas Gelf <thomas@gelf.net>
2018-10-06 16:34:23 +0200

    sql: add basket schema and migrations

--- schema/pgsql.sql
+++ schema/pgsql.sql
@@ -42,6 +42,7 @@ CREATE TYPE enum_sync_state AS ENUM(
     'failing'
 );
 CREATE TYPE enum_host_service AS ENUM('host', 'service');
+CREATE TYPE enum_owner_type AS ENUM('user', 'usergroup', 'role');
 
 
 CREATE OR REPLACE FUNCTION unix_timestamp(timestamp with time zone) RETURNS bigint AS '
@@ -69,6 +70,46 @@ CREATE INDEX activity_log_search_idx2 ON director_activity_log (object_type, obj
 CREATE INDEX activity_log_author ON director_activity_log (author);
 COMMENT ON COLUMN director_activity_log.old_properties IS 'Property hash, JSON';
 COMMENT ON COLUMN director_activity_log.new_properties IS 'Property hash, JSON';
+
+
+CREATE TABLE director_basket (
+  uuid bytea CHECK(LENGTH(uuid) = 16) NOT NULL,
+  basket_name VARCHAR(64) NOT NULL,
+  owner_type enum_owner_type NOT NULL,
+  owner_value VARCHAR(255) NOT NULL,
+  objects text NOT NULL, -- json-encoded
+  PRIMARY KEY (uuid)
+);
+
+CREATE UNIQUE INDEX basket_basket_name ON director_basket (basket_name);
+
+
+CREATE TABLE director_basket_content (
+  checksum bytea CHECK(LENGTH(checksum) = 20) NOT NULL,
+  summary VARCHAR(255) NOT NULL, -- json
+  content text NOT NULL, -- json
+  PRIMARY KEY (checksum)
+);
+
+
+CREATE TABLE director_basket_snapshot (
+  basket_uuid bytea CHECK(LENGTH(basket_uuid) = 16) NOT NULL,
+  ts_create bigint NOT NULL,
+  content_checksum bytea CHECK(LENGTH(content_checksum) = 20) NOT NULL,
+  PRIMARY KEY (basket_uuid, ts_create),
+  CONSTRAINT basked_snapshot_basket
+  FOREIGN KEY (basket_uuid)
+    REFERENCES director_basket (uuid)
+    ON DELETE CASCADE
+    ON UPDATE RESTRICT,
+  CONSTRAINT basked_snapshot_content
+  FOREIGN KEY (content_checksum)
+    REFERENCES director_basket_content (checksum)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT
+);
+
+CREATE INDEX basket_snapshot_sort_idx ON director_basket_snapshot (ts_create);
 
 
 CREATE TABLE director_generated_config (
@@ -2027,4 +2068,4 @@ CREATE TABLE icinga_timeperiod_exclude (
 
 INSERT INTO director_schema_migration
   (schema_version, migration_time)
-  VALUES (152, NOW());
+  VALUES (153, NOW());
