b624fca3691ab52fbb3b549384b60182aa938e04
Gregory Hargreaves <gphargreaves@googlemail.com>
2018-10-19 15:09:43 +0100

    Added DynamicApplyMatches implementation to remove hard coded Host implementation in GroupMembershipResolver

---
+++ library/Director/Objects/DynamicApplyMatches.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Icinga\Module\Director\Objects;
+
+class DynamicApplyMatches extends ObjectApplyMatches
+{
+    protected static $type = '';
+
+
+    public static function setType($type)
+    {
+        static::$type = $type;
+        return static::$type;
+    }
+}
