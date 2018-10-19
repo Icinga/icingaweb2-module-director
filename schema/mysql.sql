c62d5ef56cf725eb068560b15a8bce060ac3521b
Gregory Hargreaves <gphargreaves@googlemail.com>
2018-10-19 14:30:39 +0100

    Added Schema migrations for the servicegroup resolved cache.

--- schema/mysql.sql
+++ schema/mysql.sql
@@ -915,6 +915,22 @@ CREATE TABLE icinga_servicegroup_service (
     ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
+CREATE TABLE icinga_servicegroup_service_resolved (
+  servicegroup_id INT(10) UNSIGNED NOT NULL,
+  service_id INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (servicegroup_id, service_id),
+  CONSTRAINT icinga_servicegroup_service_resolved_service
+  FOREIGN KEY service (service_id)
+  REFERENCES icinga_service (id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT icinga_servicegroup_service_resolved_servicegroup
+  FOREIGN KEY servicegroup (servicegroup_id)
+  REFERENCES icinga_servicegroup (id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE icinga_hostgroup_host (
   hostgroup_id INT(10) UNSIGNED NOT NULL,
   host_id INT(10) UNSIGNED NOT NULL,
