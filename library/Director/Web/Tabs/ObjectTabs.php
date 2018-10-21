8e38b1e318f0e45207896172c7c9385f6acbc652
Gregory Hargreaves <gphargreaves@googlemail.com>
2018-10-21 19:16:53 +0100

    Fixed style issue.

--- library/Director/Web/Tabs/ObjectTabs.php
+++ library/Director/Web/Tabs/ObjectTabs.php
@@ -105,7 +105,9 @@ class ObjectTabs extends Tabs
         }
 
         // TODO: remove table check once we resolve all group types
-        if ($object->isGroup() && ($object->getShortTableName() === 'hostgroup' || $object->getShortTableName() === 'servicegroup')) {
+        if ($object->isGroup() &&
+            ($object->getShortTableName() === 'hostgroup' || $object->getShortTableName() === 'servicegroup')
+        ) {
             $this->add('membership', [
                 'url'       => sprintf('director/%s/membership', $type),
                 'urlParams' => $params,
