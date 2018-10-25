cdff318b2423f369d55f4c84ccf57fd596aba0f5
Thomas Gelf <thomas@gelf.net>
2018-10-25 08:35:46 +0200

    Services: fix multiselection / add basket link

--- application/controllers/ServicesController.php
+++ application/controllers/ServicesController.php
@@ -2,6 +2,9 @@
 
 namespace Icinga\Module\Director\Controllers;
 
+use dipl\Html\Link;
+use dipl\Web\Url;
+use Icinga\Module\Director\DirectorObject\Automation\ExportInterface;
 use Icinga\Module\Director\Web\Controller\ObjectsController;
 
 class ServicesController extends ObjectsController
@@ -11,4 +14,29 @@ class ServicesController extends ObjectsController
         'groups',
         'disabled'
     );
+
+    public function edittemplatesAction()
+    {
+        parent::editAction();
+
+        $objects = $this->loadMultiObjectsFromParams();
+        $names = [];
+        /** @var ExportInterface $object */
+        foreach ($objects as $object) {
+            $names[] = $object->getUniqueIdentifier();
+        }
+
+        $url = Url::fromPath('director/basket/add', [
+            'type'  => 'ServiceTemplate',
+        ]);
+
+        $url->getParams()->addValues('names', $names);
+
+        $this->actions()->add(Link::create(
+            $this->translate('Add to Basket'),
+            $url,
+            null,
+            ['class' => 'icon-tag']
+        ));
+    }
 }
