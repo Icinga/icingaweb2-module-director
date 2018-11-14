ed9e793e899541405660022e694cccabd9ae53f4
Thomas Gelf <thomas@gelf.net>
2018-11-14 08:33:49 +0100

    schema: prepare new daemon table

---
+++ schema/mysql-migrations/upgrade_167.sql
@@ -0,0 +1,25 @@
+CREATE TABLE director_daemon_info (
+  instance_uuid_hex VARCHAR(32) NOT NULL, -- random by daemon
+  schema_version SMALLINT UNSIGNED NOT NULL,
+  fqdn VARCHAR(255) NOT NULL,
+  username VARCHAR(64) NOT NULL,
+  pid INT UNSIGNED NOT NULL,
+  binary_path VARCHAR(128) NOT NULL,
+  binary_realpath VARCHAR(128) NOT NULL,
+  php_binary_path VARCHAR(128) NOT NULL,
+  php_binary_realpath VARCHAR(128) NOT NULL,
+  php_version VARCHAR(64) NOT NULL,
+  php_integer_size SMALLINT NOT NULL,
+  running_with_systemd ENUM('y', 'n') NOT NULL,
+  ts_started BIGINT(20) NOT NULL,
+  ts_stopped BIGINT(20) DEFAULT NULL,
+  ts_last_modification BIGINT(20) DEFAULT NULL,
+  ts_last_update BIGINT(20) DEFAULT NULL,
+  process_info MEDIUMTEXT NOT NULL,
+  PRIMARY KEY (instance_uuid_hex)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
+
+
+INSERT INTO director_schema_migration
+  (schema_version, migration_time)
+  VALUES (167, NOW());
--- schema/mysql.sql
+++ schema/mysql.sql
@@ -9,6 +9,27 @@
 
 SET sql_mode = 'STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION,PIPES_AS_CONCAT,ANSI_QUOTES,ERROR_FOR_DIVISION_BY_ZERO';
 
+CREATE TABLE director_daemon_info (
+  instance_uuid_hex VARCHAR(32) NOT NULL, -- random by daemon
+  schema_version SMALLINT UNSIGNED NOT NULL,
+  fqdn VARCHAR(255) NOT NULL,
+  username VARCHAR(64) NOT NULL,
+  pid INT UNSIGNED NOT NULL,
+  binary_path VARCHAR(128) NOT NULL,
+  binary_realpath VARCHAR(128) NOT NULL,
+  php_binary_path VARCHAR(128) NOT NULL,
+  php_binary_realpath VARCHAR(128) NOT NULL,
+  php_version VARCHAR(64) NOT NULL,
+  php_integer_size SMALLINT NOT NULL,
+  running_with_systemd ENUM('y', 'n') NOT NULL,
+  ts_started BIGINT(20) NOT NULL,
+  ts_stopped BIGINT(20) DEFAULT NULL,
+  ts_last_modification BIGINT(20) DEFAULT NULL,
+  ts_last_update BIGINT(20) DEFAULT NULL,
+  process_info MEDIUMTEXT NOT NULL,
+  PRIMARY KEY (instance_uuid_hex)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
+
 CREATE TABLE director_activity_log (
   id BIGINT(20) UNSIGNED AUTO_INCREMENT NOT NULL,
   object_type VARCHAR(64) NOT NULL,
@@ -1848,4 +1869,4 @@ CREATE TABLE icinga_scheduled_downtime_range (
 
 INSERT INTO director_schema_migration
   (schema_version, migration_time)
-  VALUES (165, NOW());
+  VALUES (167, NOW());
