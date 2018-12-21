6e32fb47ddb59ca66f68f74903553b409ee88695
lokidaibel <lokidiabel@gmail.com>
2018-12-21 14:43:36 +0100

    Create ServicegroupCommand.php
    Created this to be able to also create Servicegroups over CLI. basicly its the same file as HostGroupCommand. Tested in 1.5.1

---
+++ application/clicommands/ServicegroupCommand.php
@@ -0,0 +1,12 @@
+<?php
+namespace Icinga\Module\Director\Clicommands;
+use Icinga\Module\Director\Cli\ObjectCommand;
+/**
+ * Manage Icinga Servicegroups
+ *
+ * Use this command to show, create, modify or delete Icinga Hostgroups
+ * objects
+ */
+class ServiceGroupCommand extends ObjectCommand
+{
+}
