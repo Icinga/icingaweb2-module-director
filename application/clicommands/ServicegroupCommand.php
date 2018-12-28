537b6e207b9ab338a63f3fc46c26743e922374f0
lokidaibel <lokidiabel@gmail.com>
2018-12-28 12:28:37 +0100

    Update ServicegroupCommand.php
    Minor cosmetic changes. As requested by Thomas.

--- application/clicommands/ServicegroupCommand.php
+++ application/clicommands/ServicegroupCommand.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Icinga\Module\Director\Clicommands;
 
 use Icinga\Module\Director\Cli\ObjectCommand;
@@ -6,7 +7,7 @@ use Icinga\Module\Director\Cli\ObjectCommand;
 /**
  * Manage Icinga Servicegroups
  *
- * Use this command to show, create, modify or delete Icinga Hostgroups
+ * Use this command to show, create, modify or delete Icinga Servicegroups
  * objects
  */
 class ServiceGroupCommand extends ObjectCommand
