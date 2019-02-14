e961f0eb2a65aa3047db9128887206f55ab4ebd4
Thomas Gelf <thomas@gelf.net>
2019-02-14 21:57:10 +0100

    BasketUpload: ignore Datafields, they get special
    ...treatment

--- library/Director/DirectorObject/Automation/Basket.php
+++ library/Director/DirectorObject/Automation/Basket.php
@@ -61,6 +61,7 @@ class Basket extends DbObject implements ExportInterface
     protected function onLoadFromDb()
     {
         $this->chosenObjects = (array) Json::decode($this->get('objects'));
+        unset($this->chosenObjects['Datafield']); // Might be in old baskets
     }
 
     public function getUniqueIdentifier()
