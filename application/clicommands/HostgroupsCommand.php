b8bf12b951f5179625864ea8ada195532608bf6e
Florian Bachmann <Florian.Bachmann@UBL-IS.de>
2019-04-30 12:59:12 +0200

    add "icingacli director hostgroups" CLI command for listing hostgroups defined in Icinga Director

---
+++ application/clicommands/HostgroupsCommand.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Icinga\Module\Director\Clicommands;
+
+use Icinga\Module\Director\Cli\ObjectsCommand;
+
+/**
+ * Manage Icinga Hostgroups
+ *
+ * Use this command to list Icinga Hostgroup objects
+ */
+class HostgroupsCommand extends ObjectsCommand
+{
+}
