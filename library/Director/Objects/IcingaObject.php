4514ada9895fd9fab47f11e3dd180a4d5445c3e7
Marc DeTrano <marc@gridshield.net>
2019-08-30 11:09:19 -0600

    make sure black listed hosts for a service are returned in sorted order

--- library/Director/Objects/IcingaObject.php
+++ library/Director/Objects/IcingaObject.php
@@ -3184,6 +3184,7 @@ abstract class IcingaObject extends DbObject implements IcingaConfigRenderer
                 $bl_hosts=$this->blacklisted_hosts;
             }
         }
+	sort($bl_hosts);
         return $bl_hosts;
     }
 
