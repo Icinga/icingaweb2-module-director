102d348bca25556f2944aacd0eb0cfac8cbd55a6
Marc DeTrano <marc@gridshield.net>
2019-09-02 10:29:31 -0600

    black listed hosts sort caused problems if called when list was 'null'.  Made sort conditional

--- library/Director/Objects/IcingaObject.php
+++ library/Director/Objects/IcingaObject.php
@@ -3295,7 +3295,9 @@ abstract class IcingaObject extends DbObject implements IcingaConfigRenderer
                 $bl_hosts=$this->blacklisted_hosts;
             }
         }
-	sort($bl_hosts);
+        if ($bl_hosts) {
+            sort($bl_hosts);
+        }
         return $bl_hosts;
     }
 
