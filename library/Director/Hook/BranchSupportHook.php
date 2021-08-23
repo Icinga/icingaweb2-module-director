04f3c6363ecac2f18890ff524944617e4a6cd43d
Thomas Gelf <thomas@gelf.net>
2021-08-23 08:53:40 +0200

    BranchSupportHook: smaller changes

--- library/Director/Hook/BranchSupportHook.php
+++ library/Director/Hook/BranchSupportHook.php
@@ -2,15 +2,37 @@
 
 namespace Icinga\Module\Director\Hook;
 
+use Icinga\Authentication\Auth;
 use Icinga\Module\Director\Data\Db\DbObject;
 use Icinga\Module\Director\Db\Branch\Branch;
+use Icinga\Module\Director\Db\Branch\BranchSTore;
 use Icinga\Web\Request;
+use ipl\Html\ValidHtml;
 
 abstract class BranchSupportHook
 {
-    abstract public function getBranchForRequest(Request $request);
+    /**
+     * @param Request $request
+     * @param BranchSTore $store
+     * @param Auth $auth
+     * @return Branch
+     */
+    abstract public function getBranchForRequest(Request $request, BranchStore $store, Auth $auth);
 
-    abstract public function linkToBranch(Branch $branch, $label = null);
+    /**
+     * @param Branch $branch
+     * @param Auth $auth
+     * @param ?string $label
+     * @return ?ValidHtml
+     */
+    abstract public function linkToBranch(Branch $branch, Auth $auth, $label = null);
 
-    abstract public function linkToBranchedObject($label, Branch $branch, DbObject $object);
+    /**
+     * @param string $label
+     * @param Branch $branch
+     * @param DbObject $object
+     * @param Auth $auth
+     * @return ?ValidHtml
+     */
+    abstract public function linkToBranchedObject($label, Branch $branch, DbObject $object, Auth $auth);
 }
