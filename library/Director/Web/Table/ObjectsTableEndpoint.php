378ef5d68699da881f38bab27c9ed698e951798f
Thomas Gelf <thomas@gelf.net>
2021-09-23 07:32:35 +0200

    ObjectsTable, Infra: fix method signature
    fixes #2395

--- library/Director/Web/Table/ObjectsTableEndpoint.php
+++ library/Director/Web/Table/ObjectsTableEndpoint.php
@@ -61,7 +61,7 @@ class ObjectsTableEndpoint extends ObjectsTable
         }
     }
 
-    protected function applyObjectTypeFilter(ZfSelect $query)
+    protected function applyObjectTypeFilter(ZfSelect $query, ZfSelect $right = null)
     {
         return $query->where("o.object_type IN ('object', 'external_object')");
     }
