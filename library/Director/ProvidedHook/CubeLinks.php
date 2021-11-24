807b0f5a14b360468efb2e86efa2f5e3859dbeb9
raviks789 <33730024+raviks789@users.noreply.github.com>
2021-11-24 16:34:36 +0100

    Use hosts deployed from director as url parameters in CubeLinks
    In case there were none of the hosts showed in a dimension for the cube module, then do not show the CubeLinks for that dimension.

--- library/Director/ProvidedHook/CubeLinks.php
+++ library/Director/ProvidedHook/CubeLinks.php
@@ -2,10 +2,13 @@
 
 namespace Icinga\Module\Director\ProvidedHook;
 
+use Icinga\Application\Config;
 use Icinga\Data\Filter\Filter;
 use Icinga\Module\Cube\Cube;
 use Icinga\Module\Cube\Hook\ActionsHook;
 use Icinga\Module\Cube\Ido\IdoHostStatusCube;
+use Icinga\Module\Director\Db;
+use Icinga\Module\Director\Objects\IcingaObject;
 use Icinga\Web\View;
 
 class CubeLinks extends ActionsHook
@@ -19,6 +22,8 @@ class CubeLinks extends ActionsHook
             return;
         }
 
+        $directorHosts = array_keys(IcingaObject::loadAllByType('Host', $this->directorDb()));
+
         $cube->finalizeInnerQuery();
         $query = $cube->innerQuery()
             ->reset('columns')
@@ -28,31 +33,53 @@ class CubeLinks extends ActionsHook
         $hosts = $cube->db()->fetchCol($query);
 
         $count = count($hosts);
+        $chosenHosts = [];
         if ($count === 1) {
             $url = 'director/host/edit';
-            $params = array('name' => $hosts[0]);
+            if (in_array($hosts[0], $directorHosts)) {
+                $chosenHosts[] = $hosts[0];
+                $params = array('name' => $hosts[0]);
 
-            $title = $view->translate('Modify a host');
-            $description = sprintf(
-                $view->translate('This allows you to modify properties for "%s"'),
-                $hosts[0]
-            );
+                $title = $view->translate('Modify a host');
+                $description = sprintf(
+                    $view->translate('This allows you to modify properties for "%s" (deployed from director)'),
+                    $hosts[0]
+                );
+            }
         } else {
             $params = null;
 
             $filter = Filter::matchAny();
             foreach ($hosts as $host) {
-                $filter->addFilter(
-                    Filter::matchAny(Filter::expression('name', '=', $host))
-                );
+                if (in_array($host, $directorHosts)) {
+                    $chosenHosts[] = $host;
+                    $filter->addFilter(
+                        Filter::matchAny(Filter::expression('name', '=', $host))
+                    );
+                }
             }
 
-            $url = 'director/hosts/edit?' . $filter->toQueryString();
+            if (count($chosenHosts) == 1) {
+                $url = 'director/host/edit';
+                $params = array('name' => $chosenHosts[0]);
 
-            $title = sprintf($view->translate('Modify %d hosts'), $count);
-            $description = $view->translate(
-                'This allows you to modify properties for all chosen hosts at once'
-            );
+                $title = $view->translate('Modify a host');
+                $description = sprintf(
+                    $view->translate('This allows you to modify properties for "%s" (deployed from director)'),
+                    $chosenHosts[0]
+                );
+            } else {
+                $url = 'director/hosts/edit?' . $filter->toQueryString();
+
+                $title = sprintf($view->translate('Modify %d hosts'), count($chosenHosts));
+                $description = $view->translate(
+                    'This allows you to modify properties for all chosen hosts (deployed from director) at once'
+                );
+            }
+        }
+
+        if (! (count($chosenHosts) > 0)) {
+            return;
         }
 
         $this->addActionLink(
@@ -61,5 +88,15 @@ class CubeLinks extends ActionsHook
             $description,
             'wrench'
         );
+    }
+
+    protected function directorDb()
+    {
+        $resourceName = Config::module('director')->get('db', 'resource');
+        if (! $resourceName) {
+            return false;
+        }
+
+        return Db::fromResourceName($resourceName);
     }
 }
