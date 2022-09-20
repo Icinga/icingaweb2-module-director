25f961a4c8c7c1abd80776dc094fb06aeb228c99
Thomas Gelf <thomas@gelf.net>
2022-09-20 12:50:37 +0200

    HostController: allow to add Sets

--- application/controllers/HostController.php
+++ application/controllers/HostController.php
@@ -119,9 +119,6 @@ class HostController extends ObjectController
         $host = $this->getHostObject();
         $this->addServicesHeader();
         $this->addTitle($this->translate('Add Service Set to %s'), $host->getObjectName());
-        if ($this->showNotInBranch($this->translate('Creating Service Sets'))) {
-            return;
-        }
 
         $this->content()->add(
             IcingaServiceSetForm::load()
