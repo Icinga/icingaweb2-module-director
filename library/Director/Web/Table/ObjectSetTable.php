59d62d0ff9d2537852e6342b5b336522041a5ac1
Thomas Gelf <thomas@gelf.net>
2022-09-20 12:53:00 +0200

    ObjectSetTable: branch support

--- library/Director/Web/Table/ObjectSetTable.php
+++ library/Director/Web/Table/ObjectSetTable.php
@@ -7,11 +7,14 @@ use Icinga\Module\Director\Db;
 use gipfl\IcingaWeb2\Link;
 use gipfl\IcingaWeb2\Table\ZfQueryBasedTable;
 use gipfl\IcingaWeb2\Url;
+use Icinga\Module\Director\Db\DbSelectParenthesis;
 use Icinga\Module\Director\Restriction\FilterByNameRestriction;
 use Ramsey\Uuid\Uuid;
 
 class ObjectSetTable extends ZfQueryBasedTable
 {
+    use TableWithBranchSupport;
+
     protected $searchColumns = [
         'os.object_name',
         'os.description',
@@ -51,7 +54,8 @@ class ObjectSetTable extends ZfQueryBasedTable
 
         $url = Url::fromPath("director/${type}set", $params);
 
-        return static::tr([
+        $classes = $this->getRowClasses($row);
+        $tr = static::tr([
             static::td([
                 Link::create(sprintf(
                     $this->translate('%s (%d members)'),
@@ -61,24 +65,47 @@ class ObjectSetTable extends ZfQueryBasedTable
                 $row->description ? ': ' . $row->description : null
             ])
         ]);
+        if (! empty($classes)) {
+            $tr->getAttributes()->add('class', $classes);
+        }
+
+        return $tr;
+    }
+
+    protected function getRowClasses($row)
+    {
+        if ($row->branch_uuid !== null) {
+            return ['branch_modified'];
+        }
+        return [];
     }
 
     protected function prepareQuery()
     {
         $type = $this->getType();
 
+        $table = "icinga_${type}_set";
         $columns = [
             'id'             => 'os.id',
             'uuid'           => 'os.uuid',
+            'branch_uuid'    => '(NULL)',
             'object_name'    => 'os.object_name',
             'object_type'    => 'os.object_type',
             'assign_filter'  => 'os.assign_filter',
             'description'    => 'os.description',
             'count_services' => 'COUNT(DISTINCT o.id)',
         ];
+        if ($this->branchUuid) {
+            $columns['count_services'] = 'COUNT(DISTINCT COALESCE(o.uuid))';
+            $columns['branch_uuid'] = 'bos.branch_uuid';
+        }
+        if ($this->branchUuid) {
+            $columns = $this->branchifyColumns($columns);
+            $this->stripSearchColumnAliases();
+        }
 
         $query = $this->db()->select()->from(
-            ['os' => "icinga_${type}_set"],
+            ['os' => $table],
             $columns
         )->joinLeft(
             ['o' => "icinga_${type}"],
@@ -92,22 +119,60 @@ class ObjectSetTable extends ZfQueryBasedTable
             "${type}_set"
         );
         $nameFilter->applyToQuery($query, 'os');
-        // Disabled for now, check for correctness:
-        // $query->joinLeft(
-        //     ['osi' => "icinga_${type}_set_inheritance"],
-        //     "osi.parent_${type}_set_id = os.id",
-        //     []
-        // )->joinLeft(
-        //     ['oso' => "icinga_${type}_set"],
-        //     "oso.id = oso.${type}_set_id",
-        //     []
-        // );
-        // 'count_hosts'    => 'COUNT(DISTINCT oso.id)',
+        if ($this->branchUuid) {
+            $right = clone($query);
 
-        $query
-            ->group('os.id')
-            ->where('os.object_type = ?', 'template')
-            ->order('os.object_name');
+            /** @var Db $conn */
+            $conn = $this->connection();
+            $query->joinLeft(
+                ['bos' => "branched_$table"],
+                // TODO: PgHexFunc
+                $this->db()->quoteInto(
+                    'bos.uuid = os.uuid AND bos.branch_uuid = ?',
+                    $conn->quoteBinary($this->branchUuid->getBytes())
+                ),
+                []
+            )->where("(bos.branch_deleted IS NULL OR bos.branch_deleted = 'n')");
+            $right->joinRight(
+                ['bos' => "branched_$table"],
+                'bos.uuid = os.uuid',
+                []
+            )
+            ->where('os.uuid IS NULL')
+            ->where('bos.branch_uuid = ?', $conn->quoteBinary($this->branchUuid->getBytes()));
+            $query->group('COALESCE(os.uuid, bos.uuid)');
+            $right->group('COALESCE(os.uuid, bos.uuid)');
+
+            $query = $this->db()->select()->union([
+                'l' => new DbSelectParenthesis($query),
+                'r' => new DbSelectParenthesis($right),
+            ]);
+            $query = $this->db()->select()->from(['u' => $query]);
+            $query->order('object_name')->limit(100);
+
+            $query
+                ->group('uuid')
+                ->where('object_type = ?', 'template')
+                ->order('object_name');
+
+        } else {
+            // Disabled for now, check for correctness:
+            // $query->joinLeft(
+            //     ['osi' => "icinga_${type}_set_inheritance"],
+            //     "osi.parent_${type}_set_id = os.id",
+            //     []
+            // )->joinLeft(
+            //     ['oso' => "icinga_${type}_set"],
+            //     "oso.id = oso.${type}_set_id",
+            //     []
+            // );
+            // 'count_hosts'    => 'COUNT(DISTINCT oso.id)',
+
+            $query
+                ->group('os.uuid')
+                ->where('os.object_type = ?', 'template')
+                ->order('os.object_name');
+        }
 
         return $query;
     }
