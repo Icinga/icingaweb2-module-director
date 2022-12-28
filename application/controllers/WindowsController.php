8b2ce92681dc4a02ddb7f7e19a55d0e28d192ffd
Thomas Gelf <thomas@gelf.net>
2022-12-28 12:21:19 +0100

    WindowsController: initial import

---
+++ application/controllers/WindowsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Icinga\Module\Director\Controllers;
+
+use Icinga\Module\Director\RestApi\IcingaForWindowsApi;
+use Icinga\Module\Director\Web\Controller\ActionController;
+use Icinga\Module\Director\Windows\GeneratedDashboard;
+use Icinga\Module\Director\Windows\RemoteMenu;
+
+class WindowsController extends ActionController
+{
+    protected $isApified = true;
+
+    public function init()
+    {
+        $handler = new IcingaForWindowsApi($this->getRequest(), $this->getResponse(), $this->db());
+        if ($this->getRequest()->isApiRequest()) {
+            $handler->dispatch();
+            // TODO: Regular shutdown
+            exit;
+        } else {
+            $this->addSingleTab($this->translate('Icinga for Windows'));
+            $response = $handler->handleApiRequest();
+            if ($response instanceof RemoteMenu) {
+                $dashboard = GeneratedDashboard::create($response, $this->db());
+                $this->content()->add($dashboard);
+            }
+        }
+    }
+
+    public function indexAction()
+    {
+        // TODO: Remove actions
+    }
+
+    public function jobsAction()
+    {
+        // TODO: Remove actions
+    }
+}
