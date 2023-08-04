ed69e97ed764feee64524cec8e8601fcccf3f07a
Michael Pirogov <vbnet.ru@gmail.com>
2023-08-04 13:14:08 +0200

    migrations: fix syntax error on 187

--- schema/pgsql-migrations/upgrade_187.sql
+++ schema/pgsql-migrations/upgrade_187.sql
@@ -1,4 +1,4 @@
-ALTER TABLE import_row_modifier ADD COLUMN filter_expression text DEFAULT NULL,
+ALTER TABLE import_row_modifier ADD COLUMN filter_expression text DEFAULT NULL;
 
 INSERT INTO director_schema_migration
   (schema_version, migration_time)
