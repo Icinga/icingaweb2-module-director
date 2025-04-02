e7084968689454e4474f17e13b2919ac9b136a0b
raviks789 <raviks789@gmail.com>
2025-04-02 12:05:25 +0200

    WIP: Add items to instantiable dictionary

--- application/views/helpers/CustomFieldset.php
+++ application/views/helpers/CustomFieldset.php
@@ -1,15 +1,15 @@
 <?php
 
-use Icinga\Module\Director\Web\Form\Element\FormFieldset;
-
 /**
  * Avoid complaints about class names:
  * @codingStandardsIgnoreStart
  */
-class Zend_View_Helper_FormFieldset extends Zend_View_Helper_FormElement
+class Zend_View_Helper_CustomFieldset extends Zend_View_Helper_Fieldset
 {
     /**
      * Render the fieldset with possible nested fieldsets.
+     *
+     * @access public
      *
      * @param string $name Form name
      * @param string $content Form content
@@ -17,15 +17,19 @@ class Zend_View_Helper_FormFieldset extends Zend_View_Helper_FormElement
      *
      * @return string
      */
-    public function formFieldset($name, $content, $value, $attribs)
+    public function customFieldset($name, $content, $attribs = null)
     {
 //        // @codingStandardsIgnoreEnd
 //        if (isset($attribs['content'])) {
 //            $content .= $attribs['content'];
 //        }
 
+        $info    = $this->_getInfo($name, $content, $attribs);
+        extract($info); // name, id, value, attribs, options, listsep, disable, escape
+
         // Implementation still in progress
+        var_dump($name);die;
         return (new Zend_View_Helper_Fieldset())
-            ->fieldset($name, $content, $value, $attribs);
+            ->fieldset($name, $content, $attribs);
     }
 }
