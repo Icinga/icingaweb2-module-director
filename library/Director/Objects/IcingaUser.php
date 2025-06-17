c4ae8b8df21d11afa169b90f4b2f5e58f3e76ccb
raviks789 <raviks789@gmail.com>
2025-06-17 16:29:38 +0200

    Allow usergroup assignment using assign_filter

--- library/Director/Objects/IcingaUser.php
+++ library/Director/Objects/IcingaUser.php
@@ -2,6 +2,8 @@
 
 namespace Icinga\Module\Director\Objects;
 
+use Icinga\Data\Db\DbConnection;
+use Icinga\Module\Director\Data\PropertiesFilter;
 use Icinga\Module\Director\Db;
 use Icinga\Module\Director\DirectorObject\Automation\ExportInterface;
 use Icinga\Module\Director\Exception\DuplicateKeyException;
@@ -51,5 +53,84 @@ class IcingaUser extends IcingaObject implements ExportInterface
     public function getUniqueIdentifier()
     {
         return $this->getObjectName();
+    }
+
+    /**
+     * Enumerate properties for user objects
+     *
+     * @param ?DbConnection $connection
+     * @param $prefix
+     * @param $filter
+     *
+     * @return array
+     */
+    public static function enumProperties(DbConnection $connection = null, $prefix = '', $filter = null): array
+    {
+        $userProperties = array();
+        if ($filter === null) {
+            $filter = new PropertiesFilter();
+        }
+
+        $realProperties = array_merge(['templates'], static::create()->listProperties());
+        sort($realProperties);
+
+        if ($filter->match(PropertiesFilter::$USER_PROPERTY, 'name')) {
+            $userProperties[$prefix . 'name'] = 'name';
+        }
+
+        foreach ($realProperties as $prop) {
+            if (!$filter->match(PropertiesFilter::$USER_PROPERTY, $prop)) {
+                continue;
+            }
+
+            if (substr($prop, -3) === '_id') {
+                if ($prop === 'template_choice_id') {
+                    continue;
+                }
+                $prop = substr($prop, 0, -3);
+            }
+
+            $userProperties[$prefix . $prop] = $prop;
+        }
+
+        unset($userProperties[$prefix . 'uuid']);
+        unset($userProperties[$prefix . 'custom_endpoint_name']);
+
+        $userVars = [];
+
+        if ($connection instanceof Db) {
+            foreach ($connection->fetchDistinctUserVars() as $var) {
+                if ($filter->match(PropertiesFilter::$CUSTOM_PROPERTY, $var->varname, $var)) {
+                    if ($var->datatype) {
+                        $userVars[$prefix . 'vars.' . $var->varname] = sprintf(
+                            '%s (%s)',
+                            $var->varname,
+                            $var->caption
+                        );
+                    } else {
+                        $userVars[$prefix . 'vars.' . $var->varname] = $var->varname;
+                    }
+                }
+            }
+        }
+
+        //$properties['vars.*'] = 'Other custom variable';
+        ksort($userVars);
+
+
+        $props = mt('director', 'User properties');
+        $vars  = mt('director', 'Custom variables');
+
+        $properties = [];
+        if (! empty($userProperties)) {
+            $properties[$props] = $userProperties;
+            $properties[$props][$prefix . 'groups'] = 'Groups';
+        }
+
+        if (! empty($userVars)) {
+            $properties[$vars] = $userVars;
+        }
+
+        return $properties;
     }
 }
