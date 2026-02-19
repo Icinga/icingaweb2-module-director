Icinga/icingaweb2-module-director

commit 0a87527d0773122df53d164f7894a1d8a4855bf1

IcingaObjectHandler: Check for import loop detection during rest api updates

library/Director/RestApi/IcingaObjectHandler.php
@@ -139,6 +139,21 @@ class IcingaObjectHandler extends RequestHandler
                         ], $data);
                         $object->replaceWith(IcingaObject::createByType($type, $data, $db));
                     }
+
+                    if (in_array((int) $object->get('id'), $object->listAncestorIds())) {
+                        throw new RuntimeException(
+                            'Import loop detected for the object '
+                            . $object->getObjectName() . ' -> Imports: '
+                            . implode(', ', $object->getImports())
+                        );
+                    }
+
+                    if (in_array($object->get('object_name'), $data['imports'])) {
+                        throw new RuntimeException(
+                            'You can not import the same object into itself: ' . $object->getObjectName()
+                        );
+                    }
+
                     $this->persistChanges($object);
                     $this->sendJson($object->toPlainObject(false, true));
                 } elseif ($allowsOverrides && $type === 'service') {

commit 46517e0b25cfefdc5658aaa16302d9df0e4d88bc

IcingaObjectHandler: Fix undefined array key 'imports' error
This was introduced by the commit 0a87527d0773122df53d164f7894a1d8a4855bf1 (https://github.com/Icinga/icingaweb2-module-director/commit/0a87527d0773122df53d164f7894a1d8a4855bf1)

library/Director/RestApi/IcingaObjectHandler.php
@@ -148,7 +148,7 @@ class IcingaObjectHandler extends RequestHandler
                         );
                     }
 
-                    if (in_array($object->get('object_name'), $data['imports'])) {
+                    if (isset($data['imports']) && in_array($object->get('object_name'), $data['imports'])) {
                         throw new RuntimeException(
                             'You can not import the same object into itself: ' . $object->getObjectName()
                         );