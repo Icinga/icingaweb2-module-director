8b64bf83c87c3fde55dab77d61de9c1f7eb68bf5
Ravi Srinivasa <ravi.srinivasa@icinga.com>
2026-04-09 08:39:30 +0200

    Remove dependency checker
    Requirements are already validated during module activation, making a post-activation dependency check redundant.
    Additionally, starting with version 1.12, Icinga Director depends on the icinga-php-legacy library rather than the
    incubator module, eliminating the risk of accidentally deactivating the incubator module while Icinga Director is active.

--- application/controllers/PhperrorController.php
+++ application/controllers/PhperrorController.php
@@ -2,9 +2,6 @@
 
 namespace Icinga\Module\Director\Controllers;
 
-use Icinga\Application\Icinga;
-use Icinga\Module\Director\Application\DependencyChecker;
-use Icinga\Module\Director\Web\Table\Dependency\DependencyInfoTable;
 use Icinga\Web\Controller;
 
 class PhperrorController extends Controller
@@ -21,23 +18,5 @@ class PhperrorController extends Controller
         );
         $this->view->title = $this->translate('Unsatisfied dependencies');
         $this->view->message = sprintf($msg, PHP_VERSION);
-    }
-
-    public function dependenciesAction()
-    {
-        $checker = new DependencyChecker(Icinga::app());
-        if ($checker->satisfiesDependencies($this->Module())) {
-            $this->redirectNow('director');
-        }
-        $this->setAutorefreshInterval(15);
-        $this->getTabs()->add('error', [
-            'label' => $this->translate('Error'),
-            'url'   => $this->getRequest()->getUrl()
-        ])->activate('error');
-        $this->view->title = $this->translate('Unsatisfied dependencies');
-        $this->view->table = (new DependencyInfoTable($checker, $this->Module()))->render();
-        $this->view->message = $this->translate(
-            "Icinga Director depends on the following modules, please install/upgrade as required"
-        );
     }
 }
--- run.php
+++ run.php
@@ -1,17 +1,7 @@
 <?php
 
-use Icinga\Application\Modules\Module;
-use Icinga\Module\Director\Application\DependencyChecker;
-
 if (version_compare(PHP_VERSION, '5.6.0') < 0) {
     include __DIR__ . '/run-php5.3.php';
-    return;
-}
-
-/** @var Module $this */
-$checker = new DependencyChecker($this->app);
-if (! $checker->satisfiesDependencies($this)) {
-    include __DIR__ . '/run-missingdeps.php';
     return;
 }
 
