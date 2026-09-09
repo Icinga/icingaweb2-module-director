ec2b596fe34dca5d0221620e5a54eef90e05682a
Ravi Srinivasa <ravi.srinivasa@icinga.com>
2026-09-09 15:33:20 +0200

    Add a Custom Property object for dictionary and array properties
    A Custom Property now describes the shape of a dictionary or array
    value, separate from the plain data fields used for import sync.

--- library/Director/Dashboard/DataDashboard.php
+++ library/Director/Dashboard/DataDashboard.php
@@ -8,9 +8,10 @@ namespace Icinga\Module\Director\Dashboard;
 class DataDashboard extends Dashboard
 {
     protected $dashletNames = [
-        'Datafield',
+        'CustomVariables',
         'DatafieldCategory',
         'Datalist',
+        'Datafield',
         'Customvar'
     ];
 
