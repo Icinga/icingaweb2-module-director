6258de10a90562286af3301f1c80d80997f3f6ca
Ravi Srinivasa <ravi.srinivasa@icinga.com>
2026-09-09 15:33:37 +0200

    Add a page listing all Custom Properties
    New navigation entry and widgets show every Custom Property across
    the module in one place, and object tables and tabs now surface
    dictionary values alongside the rest of an object's variables.

---
+++ library/Director/Dashboard/Dashlet/CustomVariablesDashlet.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Icinga\Module\Director\Dashboard\Dashlet;
+
+use Icinga\Module\Director\Auth\Permission;
+
+class CustomVariablesDashlet extends Dashlet
+{
+    protected $icon = 'edit';
+
+    public function getTitle()
+    {
+        return $this->translate('Manage Custom Variables');
+    }
+
+    public function getSummary()
+    {
+        return $this->translate(
+            'Manage the custom variables required for your configuration and make sure they fit your rules'
+        );
+    }
+
+    public function getUrl()
+    {
+        return 'director/variables';
+    }
+
+    public function listRequiredPermissions()
+    {
+        return [Permission::ADMIN];
+    }
+}
