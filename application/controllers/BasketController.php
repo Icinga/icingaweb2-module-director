83097b119c76fda4e7ac1350fe46f8d24f821a8d
Ravi Srinivasa <ravi.srinivasa@icinga.com>
2026-09-14 11:20:43 +0200

    BasketController: Simplify the Add to Basket link
    Build the link with Link::create's own params argument instead of
    manually building a Url object first.

--- application/controllers/BasketController.php
+++ application/controllers/BasketController.php
@@ -6,7 +6,6 @@ use Exception;
 use gipfl\Diff\HtmlRenderer\InlineDiff;
 use gipfl\Diff\PhpDiff;
 use gipfl\IcingaWeb2\Link;
-use gipfl\IcingaWeb2\Url;
 use gipfl\Web\Table\NameValueTable;
 use gipfl\Web\Widget\Hint;
 use Icinga\Date\DateFormatter;
@@ -58,16 +57,13 @@ class BasketController extends ActionController
         );
         $basket = $this->requireBasket();
 
-        $url = Url::fromPath('director/basket/add', [
-            'type'  => 'Basket',
-        ]);
-
-        $url->getParams()->addValues('names', [$basket->getUniqueIdentifier()]);
-
         $this->actions()->add(Link::create(
             $this->translate('Add to Basket'),
-            $url,
-            null,
+            'director/basket/add',
+            [
+                'type'  => 'Basket',
+                'names' => $basket->getUniqueIdentifier()
+            ],
             ['class' => 'icon-tag']
         ));
 
