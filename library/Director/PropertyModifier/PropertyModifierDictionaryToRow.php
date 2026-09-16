08ae2c88926b1a2e01adfabbf6f601b6f517df0b
Ravi Srinivasa <ravi.srinivasa@icinga.com>
2026-09-16 09:42:22 +0200

    DictionaryToRow: Only cast empty arrays to objects
    A non-empty array here means the entry is really a list, not a
    dictionary, and casting it to an object would hide that instead of
    surfacing the existing invalid data error. Only an empty array needs
    the cast, since JSON decoding can't tell an empty object from an
    empty list.

--- library/Director/PropertyModifier/PropertyModifierDictionaryToRow.php
+++ library/Director/PropertyModifier/PropertyModifierDictionaryToRow.php
@@ -78,9 +78,10 @@ class PropertyModifierDictionaryToRow extends PropertyModifierHook
         }
         $result = [];
         foreach ($value as $key => $properties) {
-            if (is_array($properties)) {
-            $properties = (object) $properties;
+            if (is_array($properties) && empty($properties)) {
+                $properties = (object) $properties;
             }
+
             if (! is_object($properties)) {
                 throw new InvalidDataException(
                     sprintf('Nested "%s" dictionary', $key),
@@ -90,7 +91,7 @@ class PropertyModifierDictionaryToRow extends PropertyModifierHook
 
             $properties->$keyColumn = $key;
             $result[] = $properties;
-    }
+        }
 
         return $result;
     }
