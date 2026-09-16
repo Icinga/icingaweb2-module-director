ad2a1ea8783e4cd45d4c3be80e9094cc7790eff7
Ravi Kumar Kempapura Srinivasa <ravi.srinivasa@icinga.com>
2026-09-16 13:30:39 +0200

    Fix zone infinite looping (#3112)
    fixes #3108
    
    Closes #3109
    
    ---------
    
    Co-authored-by: Eric Lippmann <eric.lippmann@icinga.com>

--- library/Director/Objects/IcingaZone.php
+++ library/Director/Objects/IcingaZone.php
@@ -3,6 +3,7 @@
 namespace Icinga\Module\Director\Objects;
 
 use Icinga\Module\Director\Db;
+use Icinga\Module\Director\Exception\NestingError;
 use Icinga\Module\Director\IcingaConfig\IcingaConfig;
 use Icinga\Module\Director\IcingaConfig\IcingaConfigHelper as c;
 
@@ -67,6 +68,60 @@ class IcingaZone extends IcingaObject
     public static function setCachedGlobalZoneNames($names)
     {
         self::$globalZoneNames = $names;
+    }
+
+    /**
+     * Resolve related properties and reject cyclic parent hierarchies
+     *
+     * @return void
+     *
+     * @throws \Icinga\Exception\NotFoundError
+     * @throws NestingError
+     */
+    protected function beforeStore(): void
+    {
+        parent::beforeStore();
+
+        if ($parentId = $this->get('parent_id')) {
+            $this->assertNoZoneLoop((int) $parentId);
+        }
+    }
+
+    /**
+     * Assert that there is no zone loop
+     *
+     * @param int $parentId
+     *
+     * @return void
+     *
+     * @throws NestingError
+     */
+    protected function assertNoZoneLoop(int $parentId): void
+    {
+        $id = (int) $this->get('id');
+        $parents = [];
+        $names = [];
+        $db = $this->getDb();
+
+        foreach ($db->fetchAll($db->select()->from('icinga_zone', ['id', 'parent_id', 'object_name'])) as $zone) {
+            $parents[(int) $zone->id] = (int) $zone->parent_id;
+            $names[(int) $zone->id] = $zone->object_name;
+        }
+
+        $chain = [$this->getObjectName()];
+        $visited = [$id => true];
+
+        while ($parentId) {
+            $chain[] = $names[$parentId] ?? $parentId;
+
+            if (isset($visited[$parentId])) {
+                throw new NestingError('Loop detected: %s', implode(' -> ', $chain));
+            }
+
+            $visited[$parentId] = true;
+
+            $parentId = $parents[$parentId] ?? 0;
+        }
     }
 
     public function getRenderingZone(?IcingaConfig $config = null)
---
+++ test/php/library/Director/Objects/IcingaZoneTest.php
@@ -0,0 +1,259 @@
+<?php
+
+namespace Tests\Icinga\Module\Director\Objects;
+
+use Icinga\Module\Director\Data\Db\DbObject;
+use Icinga\Module\Director\Data\Db\DbObjectStore;
+use Icinga\Module\Director\Db\Branch\Branch;
+use Icinga\Module\Director\Exception\NestingError;
+use Icinga\Module\Director\Objects\IcingaHost;
+use Icinga\Module\Director\Objects\IcingaZone;
+use Icinga\Module\Director\Test\BaseTestCase;
+use ReflectionProperty;
+
+/**
+ * Zone hierarchy validation and recovery through the branch-aware loader
+ */
+class IcingaZoneTest extends BaseTestCase
+{
+    private ?ReflectionProperty $objectStoreProperty = null;
+
+    private ?DbObjectStore $originalObjectStore = null;
+
+    private bool $transactionStarted = false;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->skipForMissingDb();
+
+        $db = $this->getDb();
+        $db->getDbAdapter()->beginTransaction();
+        $this->transactionStarted = true;
+
+        // The public setter cannot restore the default null store.
+        $this->objectStoreProperty = new ReflectionProperty(DbObject::class, 'dbObjectStore');
+        $this->originalObjectStore = $this->objectStoreProperty->getValue();
+        DbObject::setDbObjectStore(new DbObjectStore($db, new Branch()));
+    }
+
+    public function tearDown(): void
+    {
+        try {
+            if ($this->transactionStarted) {
+                $this->getDb()->getDbAdapter()->rollBack();
+            }
+        } finally {
+            if ($this->objectStoreProperty !== null) {
+                $this->objectStoreProperty->setValue(null, $this->originalObjectStore);
+            }
+
+            parent::tearDown();
+        }
+    }
+
+    public function testParentSurvivesReload(): void
+    {
+        $parent = $this->createZone('parent');
+        $child = $this->createZone('child', $parent);
+
+        $loaded = IcingaZone::load($child->getObjectName(), $this->getDb());
+
+        self::assertEquals($parent->get('id'), $loaded->get('parent_id'));
+        self::assertSame($parent->getObjectName(), $loaded->get('parent'));
+    }
+
+    public function testHostZoneSurvivesReload(): void
+    {
+        $zone = $this->createZone('host-zone');
+        $host = IcingaHost::create([
+            'object_name' => '___TEST___zone-host',
+            'object_type' => 'object',
+            'zone_id'     => $zone->get('id'),
+        ], $this->getDb());
+        $host->store();
+
+        $loaded = IcingaHost::load($host->getObjectName(), $this->getDb());
+
+        self::assertEquals($zone->get('id'), $loaded->get('zone_id'));
+        self::assertSame($zone->getObjectName(), $loaded->get('zone'));
+    }
+
+    public function testNamedParentSurvivesCreationAndReload(): void
+    {
+        $parent = $this->createZone('parent');
+        $child = IcingaZone::create([
+            'object_name' => '___TEST___zone-child',
+            'object_type' => 'object',
+            'parent'      => $parent->getObjectName(),
+        ], $this->getDb());
+        $child->store();
+
+        $loaded = IcingaZone::load($child->getObjectName(), $this->getDb());
+
+        self::assertEquals($parent->get('id'), $loaded->get('parent_id'));
+        self::assertSame($parent->getObjectName(), $loaded->get('parent'));
+    }
+
+    public function testRejectsSelfAsParent(): void
+    {
+        $zone = $this->createZone('self');
+        $zone = IcingaZone::load($zone->getObjectName(), $this->getDb());
+        $zone->set('parent_id', $zone->get('id'));
+
+        $this->expectException(NestingError::class);
+        $this->expectExceptionMessage('Loop detected: ___TEST___zone-self -> ___TEST___zone-self');
+
+        $zone->store();
+    }
+
+    public function testRejectsDescendantAsParent(): void
+    {
+        $root = $this->createZone('root');
+        $child = $this->createZone('child', $root);
+        $grandchild = $this->createZone('grandchild', $child);
+        $root = IcingaZone::load($root->getObjectName(), $this->getDb());
+        $root->set('parent_id', $grandchild->get('id'));
+
+        $this->expectException(NestingError::class);
+        $this->expectExceptionMessage(
+            'Loop detected: ___TEST___zone-root -> ___TEST___zone-grandchild'
+            . ' -> ___TEST___zone-child -> ___TEST___zone-root'
+        );
+
+        $root->store();
+    }
+
+    public function testRejectsSelfAsNamedParent(): void
+    {
+        $zone = $this->createZone('self');
+        $zone = IcingaZone::load($zone->getObjectName(), $this->getDb());
+        $zone->set('parent', $zone->getObjectName());
+
+        $this->expectException(NestingError::class);
+        $this->expectExceptionMessage('Loop detected: ___TEST___zone-self -> ___TEST___zone-self');
+
+        $zone->store();
+    }
+
+    public function testCanLoadAndRepairExistingSelfLoop(): void
+    {
+        $zone = $this->createZone('self');
+        $this->forceParent($zone, $zone);
+
+        $loaded = IcingaZone::load($zone->getObjectName(), $this->getDb());
+        self::assertSame($zone->getObjectName(), $loaded->get('parent'));
+
+        $loaded->set('parent_id', null);
+        $loaded->store();
+
+        self::assertNull(IcingaZone::load($zone->getObjectName(), $this->getDb())->get('parent_id'));
+    }
+
+    public function testCanLoadAndRepairExistingMultiZoneLoop(): void
+    {
+        $parent = $this->createZone('parent');
+        $child = $this->createZone('child', $parent);
+        $this->forceParent($parent, $child);
+
+        $loaded = IcingaZone::load($parent->getObjectName(), $this->getDb());
+        self::assertSame($child->getObjectName(), $loaded->get('parent'));
+
+        $loaded->set('parent_id', null);
+        $loaded->store();
+
+        self::assertNull(IcingaZone::load($parent->getObjectName(), $this->getDb())->get('parent_id'));
+        self::assertSame(
+            $parent->getObjectName(),
+            IcingaZone::load($child->getObjectName(), $this->getDb())->get('parent')
+        );
+    }
+
+    public function testRejectsParentChainEnteringExistingLoop(): void
+    {
+        $parent = $this->createZone('parent');
+        $child = $this->createZone('child', $parent);
+        $this->forceParent($parent, $child);
+        $zone = $this->createZone('outside');
+        $zone = IcingaZone::load($zone->getObjectName(), $this->getDb());
+        $zone->set('parent_id', $parent->get('id'));
+
+        $this->expectException(NestingError::class);
+        $this->expectExceptionMessage(
+            'Loop detected: ___TEST___zone-outside -> ___TEST___zone-parent'
+            . ' -> ___TEST___zone-child -> ___TEST___zone-parent'
+        );
+
+        $zone->store();
+    }
+
+    public function testRejectsNewZoneEnteringExistingLoop(): void
+    {
+        $parent = $this->createZone('parent');
+        $child = $this->createZone('child', $parent);
+        $this->forceParent($parent, $child);
+
+        $this->expectException(NestingError::class);
+        $this->expectExceptionMessage(
+            'Loop detected: ___TEST___zone-outside -> ___TEST___zone-parent'
+            . ' -> ___TEST___zone-child -> ___TEST___zone-parent'
+        );
+
+        $this->createZone('outside', $parent);
+    }
+
+    public function testRejectsNewZoneWithNamedParentInExistingLoop(): void
+    {
+        $parent = $this->createZone('parent');
+        $this->forceParent($parent, $parent);
+        $zone = IcingaZone::create([
+            'object_name' => '___TEST___zone-outside',
+            'object_type' => 'object',
+            'parent'      => $parent->getObjectName(),
+        ], $this->getDb());
+
+        $this->expectException(NestingError::class);
+        $this->expectExceptionMessage(
+            'Loop detected: ___TEST___zone-outside -> ___TEST___zone-parent -> ___TEST___zone-parent'
+        );
+
+        $zone->store();
+    }
+
+    /**
+     * Create a stored zone in the current test transaction
+     *
+     * @param string $suffix
+     * @param ?IcingaZone $parent
+     *
+     * @return IcingaZone
+     */
+    private function createZone(string $suffix, ?IcingaZone $parent = null): IcingaZone
+    {
+        $zone = IcingaZone::create([
+            'object_name' => '___TEST___zone-' . $suffix,
+            'object_type' => 'object',
+            'parent_id'   => $parent === null ? null : $parent->get('id'),
+        ], $this->getDb());
+        $zone->store();
+
+        return $zone;
+    }
+
+    /**
+     * Reproduce a hierarchy stored before cycle validation was introduced
+     *
+     * @param IcingaZone $zone
+     * @param IcingaZone $parent
+     *
+     * @return void
+     */
+    private function forceParent(IcingaZone $zone, IcingaZone $parent): void
+    {
+        $this->getDb()->getDbAdapter()->update(
+            'icinga_zone',
+            ['parent_id' => $parent->get('id')],
+            ['id = ?' => $zone->get('id')]
+        );
+    }
+}
