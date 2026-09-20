0b583d2c7091205f9bc3959d3a505520e16fbe89
Daniel Vedovato <daniel.vedovato@wuerth-it.com>
2026-09-20 08:39:55 +0200

    test: reproduce missing array-shaped fields despite template declarations (#3072)

--- test/php/library/Director/Form/IcingaServiceApplyForEmptyDatafieldTest.php
+++ test/php/library/Director/Form/IcingaServiceApplyForEmptyDatafieldTest.php
@@ -6,10 +6,16 @@
 namespace Tests\Icinga\Module\Director\Form;
 
 use Icinga\Module\Director\DataType\DataTypeArray;
+use Icinga\Module\Director\DataType\DataTypeDatalist;
+use Icinga\Module\Director\DataType\DataTypeSqlQuery;
+use Icinga\Module\Director\DataType\DataTypeString;
+use Icinga\Module\Director\Db\DbUtil;
 use Icinga\Module\Director\Forms\IcingaServiceForm;
 use Icinga\Module\Director\Objects\DirectorDatafield;
+use Icinga\Module\Director\Objects\DirectorProperty;
 use Icinga\Module\Director\Objects\IcingaHost;
 use Icinga\Module\Director\Test\BaseTestCase;
+use Ramsey\Uuid\Uuid;
 
 class IcingaServiceApplyForEmptyDatafieldTest extends BaseTestCase
 {
@@ -61,4 +67,124 @@ class IcingaServiceApplyForEmptyDatafieldTest extends BaseTestCase
             $host->delete();
         }
     }
+    public function testAttachedArrayValuedDatafieldsWithNoHostValueAreSelectable(): void
+    {
+        if ($this->skipForMissingDb()) {
+            return;
+        }
+
+        $db = $this->getDb();
+        $dba = $db->getDbAdapter();
+        $host = IcingaHost::create([
+            'object_name' => '___TEST___array_field_types_3072',
+            'object_type' => 'template',
+        ], $db);
+        $host->store();
+        $fields = [];
+
+        try {
+            foreach ([
+                'datalist_array' => [DataTypeDatalist::class, 'array'],
+                'sqlquery_array' => [DataTypeSqlQuery::class, 'array'],
+                'datalist_scalar' => [DataTypeDatalist::class, 'string'],
+                'sqlquery_scalar' => [DataTypeSqlQuery::class, 'string'],
+                'plain_scalar' => [DataTypeString::class, null],
+            ] as $suffix => [$type, $valueType]) {
+                $field = DirectorDatafield::create([
+                    'varname' => '___TEST___3072_' . $suffix,
+                    'caption' => $suffix,
+                    'datatype' => $type,
+                ], $db);
+                if ($valueType !== null) {
+                    $field->setSettings(['data_type' => $valueType]);
+                }
+                $field->store();
+                $fields[] = $field;
+                $dba->insert('icinga_host_field', [
+                    'host_id' => $host->get('id'),
+                    'datafield_id' => $field->get('id'),
+                    'is_required' => 'n',
+                ]);
+            }
+
+            $options = array_values(self::callMethod(
+                IcingaServiceForm::load()->setDb($db),
+                'applyForVars',
+                []
+            ))[0];
+
+            $this->assertArrayHasKey('host.vars.___TEST___3072_datalist_array', $options);
+            $this->assertArrayHasKey('host.vars.___TEST___3072_sqlquery_array', $options);
+            $this->assertArrayNotHasKey('host.vars.___TEST___3072_datalist_scalar', $options);
+            $this->assertArrayNotHasKey('host.vars.___TEST___3072_sqlquery_scalar', $options);
+            $this->assertArrayNotHasKey('host.vars.___TEST___3072_plain_scalar', $options);
+        } finally {
+            foreach ($fields as $field) {
+                $field->delete();
+            }
+            $host->delete();
+        }
+    }
+
+    public function testUnpopulatedArrayPropertyShapesAreSelectable(): void
+    {
+        if ($this->skipForMissingDb()) {
+            return;
+        }
+
+        $db = $this->getDb();
+        $dba = $db->getDbAdapter();
+        $host = IcingaHost::create([
+            'object_name' => '___TEST___array_property_types_3072',
+            'object_type' => 'template',
+        ], $db);
+        $host->store();
+        $properties = [];
+
+        try {
+            foreach ([
+                'fixed_array' => ['fixed-array', null],
+                'datalist_array' => ['datalist-strict', 'dynamic-array'],
+                'datalist_scalar' => ['datalist-strict', 'string'],
+                'plain_scalar' => ['string', null],
+            ] as $suffix => [$type, $itemType]) {
+                $property = DirectorProperty::create([
+                    'uuid' => Uuid::uuid4()->getBytes(),
+                    'key_name' => '___TEST___3072_property_' . $suffix,
+                    'value_type' => $type,
+                ], $db);
+                $property->store();
+                $properties[] = $property;
+                if ($itemType !== null) {
+                    DirectorProperty::create([
+                        'uuid' => Uuid::uuid4()->getBytes(),
+                        'parent_uuid' => $property->get('uuid'),
+                        'key_name' => '0',
+                        'value_type' => $itemType,
+                    ], $db)->store();
+                }
+                $dba->insert('icinga_host_property', [
+                    'host_uuid' => DbUtil::quoteBinaryCompat(DbUtil::binaryResult($host->get('uuid')), $dba),
+                    'property_uuid' => DbUtil::quoteBinaryCompat($property->get('uuid'), $dba),
+                ]);
+            }
+
+            $options = array_values(self::callMethod(
+                IcingaServiceForm::load()->setDb($db),
+                'applyForVars',
+                []
+            ))[0];
+
+            $this->assertArrayHasKey('host.vars.___TEST___3072_property_fixed_array', $options);
+            $this->assertArrayHasKey('host.vars.___TEST___3072_property_datalist_array', $options);
+            $this->assertArrayNotHasKey('host.vars.___TEST___3072_property_datalist_scalar', $options);
+            $this->assertArrayNotHasKey('host.vars.___TEST___3072_property_plain_scalar', $options);
+        } finally {
+            foreach ($properties as $property) {
+                $property->delete();
+            }
+            $host->delete();
+        }
+    }
+
 }
