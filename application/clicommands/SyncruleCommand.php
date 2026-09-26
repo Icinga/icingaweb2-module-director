8f44f1e4cb7d600321d0acd97c0290163e850a26
Donien <88634789+Donien@users.noreply.github.com>
2026-09-26 18:56:11 +0200

    Correctly display results of syncrule in CLI
    When checking a syncrule via CLI, the information of how many objects
    would be created, modified, and deleted has been incorrect.
    The objects created and modified were switched.
    This information will now be displayed correctly.

--- application/clicommands/SyncruleCommand.php
+++ application/clicommands/SyncruleCommand.php
@@ -74,8 +74,8 @@ class SyncruleCommand extends Command
             $mods = $this->getExpectedModificationCounts($rule);
             printf(
                 "Expected modifications: %dx create, %dx modify, %dx delete\n",
-                $mods->modify,
                 $mods->create,
+                $mods->modify,
                 $mods->delete
             );
         }
